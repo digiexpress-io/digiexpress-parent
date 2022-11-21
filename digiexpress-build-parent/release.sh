b1833f61529003187c2ac39ac2c03b6700439293
Olev Mutso <olev.mutso@resys.io>
2022-11-21 17:25:19 +0200

    Update release.sh

--- digiexpress-build-parent/release.sh
+++ digiexpress-build-parent/release.sh
@@ -51,7 +51,7 @@ git config --global user.email "$BOT_EMAIL";
 git commit -am "release: ${RELEASE_VERSION}"
 git tag -a ${RELEASE_VERSION} -m "release ${RELEASE_VERSION}"
 
-mvn clean deploy -Pdigiexpress-release --settings digiexpress-build-parent/ci-maven-settings.xml -B -Dmaven.javadoc.skip=false -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+mvn clean deploy --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED -Pdigiexpress-release --settings digiexpress-build-parent/ci-maven-settings.xml -B -Dmaven.javadoc.skip=false -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 mvn versions:set -DnewVersion=${PROJECT_VERSION}
 git commit -am "release: ${RELEASE_VERSION}"
 git push
