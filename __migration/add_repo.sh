adc15196a69d1a1291a1368f0429533d5413f0ce
Olev Mutso <olev.mutso@resys.io>
2023-07-14 15:18:30 +0300

    migration setup

---
+++ __migration/add_repo.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+echo "Input repository url?"
+read ssh_repo_url
+
+echo "Input repository branch?"
+read ssh_repo_branch
+
+echo "Project nature: 'mvn' or 'ts'?"
+read repo_nature
+
+if [ "$repo_nature" != 'mvn' ] || [ "$repo_nature" != 'ts' ]; then
+  echo "unsupported option ... $repo_nature"
+  exit
+fi
+
+
+ssh_repo_name=$(basename $ssh_repo_url .git)
+repo_location="__migration/cloned_repos/$ssh_repo_name"
+echo "Using migration config:"
+echo "=============================================="
+echo "repository will be cloned: $ssh_repo_url"
+echo "repository branch: $ssh_repo_branch"
+echo "repository basename: $ssh_repo_name"
+echo "repository location: $repo_location"
+echo "=============================================="
+echo "Type 'yes' for continue"
+read confirmation
+
+
+
+
+if [ "$confirmation" != 'yes' ]; then
+  echo "Cancelling ..."
+  exit
+fi                            
+
+
+cd "cloned_repos"
+git clone $ssh_repo_url
+git checkout $ssh_repo_branch
+cd "../../"
+
+echo "=============================================="
+echo "Repo added:"
+echo "location: $repo_location"
+echo "currently at: $(pwd)"
+echo "=============================================="
+echo "Type 'yes' for continue"
+read confirmation
+
+
+if [ "$confirmation" != 'yes' ]; then
+  echo "Cancelling ..."
+  exit
+fi                            
+
+
+
+git remote add -f $ssh_repo_name $repo_location
+git merge "$ssh_repo_name/$ssh_repo_branch"  --no-commit --allow-unrelated-histories
+
+
+echo "=============================================="
+new_file_location=""
+if [ "$repo_nature" != 'mvn' ]; then
+  new_file_location='mvn_setup/$ssh_repo_name/'
+fi
+if [ "$repo_nature" != 'ts' ]; then
+  new_file_location='ts_setup/$ssh_repo_name/'
+fi
+
+git_move_command="file to be moved: "
+for file_name in $(ls -1a);
+do 	    
+  if [ "$file_name" == '.git' ] || 
+     [ "$file_name" == '__migration' ] || 
+  	 [ "$file_name" == 'mvn_setup' ] ||
+  	 [ "$file_name" == 'ts_setup' ] ||
+  	 [ "$file_name" == 'bazel_setup' ] || 
+  	 [ "$file_name" == '.' ] ||
+  	 [ "$file_name" == '..' ]; then
+    continue                    
+  fi                            
+  printf "$git_move_command$file_name\n";
+  git mv $file_name $file_name
+done
+
+git commit -am "repo: '$ssh_repo_url', branch: '$ssh_repo_branch' migrated to: '$new_file_location'"
+
+
