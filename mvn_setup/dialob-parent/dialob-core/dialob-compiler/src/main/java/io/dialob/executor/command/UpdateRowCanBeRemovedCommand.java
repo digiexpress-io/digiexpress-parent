cb8057b973226307dbc00e65327f1fcccb7d9647
Olev Mutso <olev.mutso@resys.io>
2023-07-24 15:16:59 +0300

    ADD_ROW and DELETE_ROW action rules

---
+++ mvn_setup/dialob-parent/dialob-core/dialob-compiler/src/main/java/io/dialob/executor/command/UpdateRowCanBeRemovedCommand.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2015 - 2021 ReSys (info@dialob.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.dialob.executor.command;
+
+import javax.annotation.Nonnull;
+
+import org.immutables.value.Value;
+
+import io.dialob.executor.model.ItemState;
+import io.dialob.program.EvalContext;
+
+@Value.Immutable
+public interface UpdateRowCanBeRemovedCommand extends AbstractUpdateBooleanAttributeCommand {
+
+
+//  @NotNull
+//  @Override
+//  default Set<EventMatcher> getEventMatchers() {
+//
+//    Set<EventMatcher> eventMatchers = getExpression().getEvalRequiredConditions();
+//    if (getTargetId().isPartial()) {
+//      return Sets.union(eventMatchers, ImmutableSet.of(
+//        EventMatchers.whenItemAdded(getTargetId()),
+//        EventMatchers.whenItemsChanged(getTargetId().getParent().get())
+//      ));
+//    }
+//    return eventMatchers;
+//
+//  }
+
+  @Nonnull
+  @Override
+  default ItemState update(@Nonnull EvalContext context, @Nonnull ItemState itemState) {
+    return itemState.update()
+      .setRowCanBeRemoved(evalExpression(context)).get();
+  }
+
+}
