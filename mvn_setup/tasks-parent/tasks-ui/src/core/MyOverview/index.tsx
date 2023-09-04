452ee226a8155b3b7a46b5fa0c70e52fed168e7d
admirkrilasevic <admirkrilasevic@gmail.com>
2023-09-04 14:14:43 +0200

    my overview graph and labels cleanup

--- mvn_setup/tasks-parent/tasks-ui/src/core/MyOverview/index.tsx
+++ mvn_setup/tasks-parent/tasks-ui/src/core/MyOverview/index.tsx
@@ -1,50 +1,52 @@
 import React from 'react';
 import { Box, Grid, Stack, Typography } from '@mui/material';
 import CircleIcon from '@mui/icons-material/Circle';
-import { PieChart, Pie, Cell} from 'recharts';
+import { PieChart, Pie, Cell } from 'recharts';
 import { FormattedMessage } from 'react-intl';
 
 import SampleReport from './reporting-sample';
 import { createReport } from './reporting-types';
 
 
-
 const MyWork: React.FC<{}> = () => {
 
   const data = createReport(SampleReport());
 
-  console.log(data);
-
   return (
-    <Grid container>
-      <Grid item lg={6}>
-          <Typography variant='h3'><FormattedMessage id='core.myOverview.title'/></Typography>
-
-        <PieChart width={250} height={250}>
-          <Pie data={data.groups} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#82ca9d">
-          </Pie>
-          <Pie data={data.events}
-            dataKey="value" cx="50%" cy="50%"
-            innerRadius={70} outerRadius={90} fill="#82ca9d">
-
-            {data.events.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={entry.color} />
+    <>
+      <Typography variant='h3'><FormattedMessage id='core.myOverview.title' /></Typography>
+      <Grid container>
+        <Grid item lg={4} md={4} sm={12} xs={12}>
+          <PieChart width={300} height={300}>
+            <Pie data={data.groups} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#82ca9d">
+              {data.groups.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.color} />
+              ))}
+            </Pie>
+            <Pie data={data.events}
+              dataKey="value" cx="50%" cy="50%"
+              innerRadius={70} outerRadius={100} fill="#82ca9d" label>
+              {data.events.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.color} />
+              ))}
+            </Pie>
+          </PieChart>
+        </Grid>
+        <Grid item lg={8} md={8} sm={12} xs={12} alignSelf='center'>
+          <Stack spacing={1}>
+            {data.groups.flatMap(group => group.events).map(item => (
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <CircleIcon sx={{ color: item.color }} />
+                <Typography variant='body1'>
+                  <FormattedMessage id={`core.myOverview.${item.type}`} />
+                </Typography>
+              </Box>
             ))}
-          </Pie>
-
-        </PieChart>
+          </Stack>
+        </Grid>
       </Grid>
-      <Grid item lg={6}>
-        <Stack spacing={1}>
-          {data.groups.flatMap(group => group.events).map(item => (
-            <Box><CircleIcon sx={{ color: item.color }} />{item.type} {item.value}</Box>
-          ))}
-        </Stack>
-
-      </Grid>
-    </Grid>
+    </>
   );
 }
-
 
 export default MyWork;
