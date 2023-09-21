886cb2683173084258ea0d60cfd116ecbd94cccb
emir.karaman <emir.karaman@resys.io>
2023-09-21 15:04:28 +0200

    implemented new style for task table assignees popover

--- mvn_setup/tasks-parent/tasks-ui/src/core/SearchField/index.tsx
+++ mvn_setup/tasks-parent/tasks-ui/src/core/SearchField/index.tsx
@@ -1,7 +1,7 @@
-import { InputAdornment, TextField } from "@mui/material";
+import { InputAdornment, SxProps, TextField } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 
-const SearchField: React.FC<{ onChange: (value: string) => void }> = ({ onChange }) => {
+const SearchField: React.FC<{ onChange: (value: string) => void, searchFieldSx?: SxProps }> = ({ onChange, searchFieldSx }) => {
   return (
     <TextField
       InputProps={{
@@ -15,6 +15,7 @@ const SearchField: React.FC<{ onChange: (value: string) => void }> = ({ onChange
       variant='standard'
       placeholder='Search'
       onChange={(e) => onChange(e.target.value)}
+      sx={searchFieldSx}
     />
   );
 }
