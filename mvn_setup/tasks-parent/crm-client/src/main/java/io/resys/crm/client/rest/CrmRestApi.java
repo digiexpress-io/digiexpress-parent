10cb89f74b26e74e76aa690b9b5a99f07db65a8b
jocelyn.mutso <jocelyn.mutso@resys.io>
2023-11-26 10:51:43 +0200

    customers rest api endpoints

--- mvn_setup/tasks-parent/crm-client/src/main/java/io/resys/crm/client/rest/CrmRestApi.java
+++ mvn_setup/tasks-parent/crm-client/src/main/java/io/resys/crm/client/rest/CrmRestApi.java
@@ -7,27 +7,32 @@ import io.resys.crm.client.api.model.CustomerCommand.CreateCustomer;
 import io.resys.crm.client.api.model.CustomerCommand.CustomerUpdateCommand;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 
 public interface CrmRestApi {
   
-  @GET @Path("tenants") @Produces(MediaType.APPLICATION_JSON)
-  Uni<List<Customer>> findCustomers();
+  @GET @Path("customers") @Produces(MediaType.APPLICATION_JSON)
+  Uni<List<Customer>> findAllCustomers();
   
-  @POST @Path("tenants") @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON)
-  Uni<List<Customer>> createCustomer(List<CreateCustomer> commands);
+  @GET @Path("customers") @Produces(MediaType.APPLICATION_JSON)
+  Uni<List<Customer>> findAllCustomersByName(@QueryParam("name") String name);
   
-  @PUT @Path("tenants") @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON)
-  Uni<List<Customer>> updateCustomer(List<CustomerUpdateCommand> commands);
-  
+  @POST @Path("customers") @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON)
+  Uni<Customer> createCustomer(CreateCustomer command);
 
-  @PUT @Path("tenants/{tenantConfigId}") @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON)
-  Uni<Customer> updateOneCustomer(@PathParam("tenantConfigId") String crmId, List<CustomerUpdateCommand> commands);
+  @PUT @Path("customers/{customerId}") @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON)
+  Uni<Customer> updateCustomer(@PathParam("customerId") String customerId, List<CustomerUpdateCommand> commands);
+  
+  @DELETE @Path("customers/{customerId}") @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON)
+  Uni<Customer> deleteCustomer(@PathParam("customerId") String customerId, CustomerUpdateCommand command);
+  
 }
