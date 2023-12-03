475ad1c62164257685bf2019a5668c8891b1cdad
Olev Mutso <olev.mutso@resys.io>
2023-12-03 12:12:32 +0200

    sys-config

---
+++ mvn_setup/tasks-parent/sysconfig-client/src/main/java/io/resys/sysconfig/client/spi/actions/CreateSysConfigActionImpl.java
@@ -0,0 +1,62 @@
+package io.resys.sysconfig.client.spi.actions;
+
+/*-
+ * #%L
+ * thena-tasks-client
+ * %%
+ * Copyright (C) 2021 - 2023 Copyright 2021 ReSys OÜ
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.Arrays;
+
+import io.resys.sysconfig.client.api.AssetClient;
+import io.resys.sysconfig.client.api.SysConfigClient.CreateSysConfigAction;
+import io.resys.sysconfig.client.api.model.SysConfig;
+import io.resys.sysconfig.client.api.model.SysConfigCommand.CreateSysConfig;
+import io.resys.sysconfig.client.api.model.SysConfigCommand.CreateSysConfigRelease;
+import io.resys.sysconfig.client.api.model.SysConfigDeployment;
+import io.resys.sysconfig.client.api.model.SysConfigDeploymentCommand.CreateSysConfigDeployment;
+import io.resys.sysconfig.client.api.model.SysConfigRelease;
+import io.resys.sysconfig.client.spi.store.DocumentStore;
+import io.resys.sysconfig.client.spi.visitors.CreateSysConfigVisitor;
+import io.resys.sysconfig.client.spi.visitors.SysConfigReleaseVisitor;
+import io.smallrye.mutiny.Uni;
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+public class CreateSysConfigActionImpl implements CreateSysConfigAction {
+  private final DocumentStore ctx;
+  private final AssetClient assetClient;
+  
+  @Override
+  public Uni<SysConfig> createOne(CreateSysConfig command) {
+    return ctx.getConfig().accept(new CreateSysConfigVisitor(Arrays.asList(command)))
+       .onItem().transform(tasks -> tasks.get(0));
+  }
+
+  @Override
+  public Uni<SysConfigDeployment> createOne(CreateSysConfigDeployment command) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Uni<SysConfigRelease> createOne(CreateSysConfigRelease command) {
+    return ctx.getConfig().accept(new SysConfigReleaseVisitor(command, ctx, assetClient))
+        .onItem().transformToUni(uni -> uni);
+  }
+}
---
+++ mvn_setup/tasks-parent/sysconfig-client/src/main/java/io/resys/sysconfig/client/spi/visitors/SysConfigReleaseVisitor.java
@@ -0,0 +1,99 @@
+package io.resys.sysconfig.client.spi.visitors;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.resys.sysconfig.client.api.AssetClient;
+import io.resys.sysconfig.client.api.model.Document;
+import io.resys.sysconfig.client.api.model.ImmutableSysConfig;
+import io.resys.sysconfig.client.api.model.SysConfig;
+import io.resys.sysconfig.client.api.model.SysConfigCommand.CreateSysConfigRelease;
+import io.resys.sysconfig.client.api.model.SysConfigRelease;
+import io.resys.sysconfig.client.spi.store.DocumentConfig;
+import io.resys.sysconfig.client.spi.store.DocumentConfig.DocObjectVisitor;
+import io.resys.sysconfig.client.spi.store.DocumentStore;
+import io.resys.sysconfig.client.spi.store.DocumentStoreException;
+import io.resys.thena.docdb.api.actions.DocCommitActions.CreateOneDoc;
+import io.resys.thena.docdb.api.actions.DocCommitActions.ModifyOneDocBranch;
+import io.resys.thena.docdb.api.actions.DocQueryActions.DocObjectsQuery;
+import io.resys.thena.docdb.api.models.QueryEnvelope;
+import io.resys.thena.docdb.api.models.QueryEnvelope.QueryEnvelopeStatus;
+import io.resys.thena.docdb.api.models.ThenaDocObject.Doc;
+import io.resys.thena.docdb.api.models.ThenaDocObject.DocBranch;
+import io.resys.thena.docdb.api.models.ThenaDocObject.DocCommit;
+import io.resys.thena.docdb.api.models.ThenaDocObject.DocLog;
+import io.resys.thena.docdb.api.models.ThenaDocObjects.DocObject;
+import io.resys.thena.projects.client.spi.store.MainBranch;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.json.JsonObject;
+
+
+public class SysConfigReleaseVisitor implements DocObjectVisitor<Uni<SysConfigRelease>> {
+  private final AssetClient assetClient;
+  private final DocumentStore ctx;
+  private final ModifyOneDocBranch updateBuilder;
+  private final CreateOneDoc createBuilder;
+  private final CreateSysConfigRelease command;
+  
+  public SysConfigReleaseVisitor(CreateSysConfigRelease command, DocumentStore ctx, AssetClient assetClient) {
+    super();
+
+    this.ctx = ctx;
+    this.command = command;
+    
+    this.assetClient = assetClient;
+    
+    final var config = ctx.getConfig();
+    this.updateBuilder = config.getClient().doc().commit().modifyOneBranch()
+        .repoId(config.getRepoId())
+        .message("Update config for release")
+        .author(config.getAuthor().get());
+    this.createBuilder = config.getClient().doc().commit().createOneDoc()
+        .repoId(config.getRepoId())
+        .docType(Document.DocumentType.SYS_CONFIG_RELEASE.name())
+        .message("Create new release")
+        .author(config.getAuthor().get())
+        .branchName(config.getBranchName());
+  }
+
+  @Override
+  public DocObjectsQuery start(DocumentConfig config, DocObjectsQuery builder) {
+    return builder.matchIds(Arrays.asList(command.getId())).branchName(MainBranch.HEAD_NAME);
+  }
+
+  @Override
+  public DocObject visitEnvelope(DocumentConfig config, QueryEnvelope<DocObject> envelope) {
+    if(envelope.getStatus() != QueryEnvelopeStatus.OK) {
+      throw DocumentStoreException.builder("GET_SYS_CONFIG_FOR_CREATING_RELEASE_FAIL")
+        .add(config, envelope)
+        .add((callback) -> callback.addArgs(JsonObject.mapFrom(command).encode()))
+        .build();
+    }
+    final var result = envelope.getObjects();
+    if(result == null || result.getDoc() == null) {
+      throw DocumentStoreException.builder("GET_SYS_CONFIG_FOR_CREATING_RELEASE_NOT_FOUND")   
+        .add(config, envelope)
+        .add((callback) -> callback.addArgs(JsonObject.mapFrom(command).encode()))
+        .build();
+    }
+    return result;
+  }
+
+  @Override
+  public Uni<SysConfigRelease> end(DocumentConfig config, DocObject blob) {
+    final var sysConfig = blob.accept((Doc doc, DocBranch docBranch, DocCommit commit, List<DocLog> log) -> docBranch.getValue().mapTo(ImmutableSysConfig.class))
+        .stream().findFirst().get();
+  
+    return assetClient.withTenantConfig(sysConfig.getTenantId())
+        .onItem().transformToUni(client -> doInAssetClient(client, sysConfig));
+  }
+  
+  private Uni<SysConfigRelease> doInAssetClient(AssetClient client, SysConfig config) {
+    final var dialobs = config.getServices().stream().map(e -> e.getFormId()).toList();
+
+    return client.assetQuery().getDialobAssets(dialobs).onItem().transform(data -> {
+      
+      return null;
+    });
+  }
+}
