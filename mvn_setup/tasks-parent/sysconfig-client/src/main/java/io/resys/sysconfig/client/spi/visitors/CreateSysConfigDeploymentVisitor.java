b4c40f800961a231992553e65a1cca0e551ceb42
Olev Mutso <olev.mutso@resys.io>
2023-12-04 14:06:07 +0200

    asset deployment

---
+++ mvn_setup/tasks-parent/sysconfig-client/src/main/java/io/resys/sysconfig/client/spi/visitors/CreateSysConfigDeploymentVisitor.java
@@ -0,0 +1,81 @@
+package io.resys.sysconfig.client.spi.visitors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import io.resys.sysconfig.client.api.model.Document;
+import io.resys.sysconfig.client.api.model.ImmutableSysConfigDeployment;
+import io.resys.sysconfig.client.api.model.SysConfigDeployment;
+import io.resys.sysconfig.client.api.model.SysConfigDeploymentCommand.CreateSysConfigDeployment;
+import io.resys.sysconfig.client.spi.store.DocumentConfig;
+import io.resys.sysconfig.client.spi.store.DocumentConfig.DocCreateVisitor;
+import io.resys.sysconfig.client.spi.store.DocumentStoreException;
+import io.resys.sysconfig.client.spi.visitors.SysConfigDeploymentCommandVisitor.NoChangesException;
+import io.resys.thena.docdb.api.actions.CommitActions.CommitResultStatus;
+import io.resys.thena.docdb.api.actions.DocCommitActions.CreateManyDocs;
+import io.resys.thena.docdb.api.actions.DocCommitActions.ManyDocsEnvelope;
+import io.resys.thena.docdb.api.models.ThenaDocObject.DocBranch;
+import io.vertx.core.json.JsonObject;
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+public class CreateSysConfigDeploymentVisitor implements DocCreateVisitor<SysConfigDeployment> {
+  private final List<? extends CreateSysConfigDeployment> commands;
+  private final List<SysConfigDeployment> entities = new ArrayList<SysConfigDeployment>();
+  
+  @Override
+  public CreateManyDocs start(DocumentConfig config, CreateManyDocs builder) {
+    builder
+      .docType(Document.DocumentType.SYS_CONFIG_DEPLOYMENT.name())
+      .author(config.getAuthor().get())
+      .message("creating sys config deployment");
+    
+    for(final var command : commands) {
+      try {
+        final var entity = new SysConfigDeploymentCommandVisitor(config).visitTransaction(Arrays.asList(command));
+        final var json = JsonObject.mapFrom(entity);
+        builder.item()
+          .append(json)
+          .docId(entity.getId())
+          .next();
+        entities.add(entity);
+      } catch (NoChangesException e) {
+        throw new RuntimeException(e.getMessage(), e);
+      }
+    }
+    return builder;
+  }
+
+  @Override
+  public List<DocBranch> visitEnvelope(DocumentConfig config, ManyDocsEnvelope envelope) {
+    if(envelope.getStatus() == CommitResultStatus.OK) {
+      return envelope.getBranch();
+    }
+    throw new DocumentStoreException("SYS_CONFIG_DEPLOYMENT_CREATE_FAIL", DocumentStoreException.convertMessages(envelope));
+  }
+
+  @Override
+  public List<SysConfigDeployment> end(DocumentConfig config, List<DocBranch> branches) {
+    final Map<String, SysConfigDeployment> configsById = new HashMap<>(
+        this.entities.stream().collect(Collectors.toMap(e -> e.getId(), e -> e)));
+    
+    branches.forEach(branch -> {
+      
+      final var next = ImmutableSysConfigDeployment.builder()
+          .from(configsById.get(branch.getDocId()))
+          .version(branch.getCommitId())
+          .build();
+      
+      configsById.put(next.getId(), next);
+    });
+    
+    return Collections.unmodifiableList(new ArrayList<>(configsById.values()));
+  }
+
+}
---
+++ mvn_setup/tasks-parent/sysconfig-client/src/main/java/io/resys/sysconfig/client/spi/visitors/SysConfigDeploymentCommandVisitor.java
@@ -0,0 +1,125 @@
+package io.resys.sysconfig.client.spi.visitors;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import io.resys.sysconfig.client.api.model.Document.DocumentType;
+import io.resys.sysconfig.client.api.model.ImmutableSysConfigDeployment;
+import io.resys.sysconfig.client.api.model.ImmutableSysConfigDeploymentTransaction;
+import io.resys.sysconfig.client.api.model.SysConfigDeployment;
+import io.resys.sysconfig.client.api.model.SysConfigDeploymentCommand;
+import io.resys.sysconfig.client.api.model.SysConfigDeploymentCommand.CreateSysConfigDeployment;
+import io.resys.sysconfig.client.api.model.SysConfigDeploymentCommand.UpdateSysConfigDeploymentDisabled;
+import io.resys.sysconfig.client.api.model.SysConfigDeploymentCommand.UpdateSysConfigDeploymentLiveDate;
+import io.resys.sysconfig.client.spi.store.DocumentConfig;
+
+
+public class SysConfigDeploymentCommandVisitor {
+  private final DocumentConfig ctx;
+  private final SysConfigDeployment start;
+  private final List<SysConfigDeploymentCommand> visitedCommands = new ArrayList<>();
+  private ImmutableSysConfigDeployment current;
+  
+  public SysConfigDeploymentCommandVisitor(DocumentConfig ctx) {
+    this.start = null;
+    this.current = null;
+    this.ctx = ctx;
+  }
+  
+  public SysConfigDeploymentCommandVisitor(SysConfigDeployment start, DocumentConfig ctx) {
+    this.start = start;
+    this.current = ImmutableSysConfigDeployment.builder().from(start).build();
+    this.ctx = ctx;
+  }
+  
+  public SysConfigDeployment visitTransaction(List<? extends SysConfigDeploymentCommand> commands) throws NoChangesException {
+    commands.forEach(this::visitCommand);
+    
+    if(visitedCommands.isEmpty()) {
+      throw new NoChangesException();
+    }
+    
+    final var transactions = new ArrayList<>(start == null ? Collections.emptyList() : start.getTransactions());
+    final var id = String.valueOf(transactions.size() +1);
+    transactions
+      .add(ImmutableSysConfigDeploymentTransaction.builder()
+        .id(id)
+        .commands(visitedCommands)
+        .build());
+    this.current = this.current.withVersion(id).withTransactions(transactions);
+    return this.current;
+  }
+  
+  private SysConfigDeployment visitCommand(SysConfigDeploymentCommand command) {
+    switch (command.getCommandType()) {
+    case CreateDeployment: return visitCreateSysConfig((CreateSysConfigDeployment) command);
+    case UpdateDeploymentDisabled: return visitUpdateDeploymentDisabled((UpdateSysConfigDeploymentDisabled) command);
+    case UpdateDeploymentLiveDate: return visitUpdateDeploymentLiveDate((UpdateSysConfigDeploymentLiveDate) command);
+    }
+    throw new SysConfigDeploymentVisitorException(String.format("Unsupported command type: %s, body: %s", command.getClass().getSimpleName(), command.toString()));
+  }
+  
+  private SysConfigDeployment visitCreateSysConfig(CreateSysConfigDeployment command) {
+    final var id = ctx.getGid().getNextId(DocumentType.SYS_CONFIG);
+    final var version = ctx.getGid().getNextVersion(DocumentType.SYS_CONFIG);
+    final var targetDate = requireTargetDate(command);
+    
+    this.current = ImmutableSysConfigDeployment.builder()
+      .id(id)
+      .created(targetDate)
+      .updated(targetDate)
+      .liveDate(command.getLiveDate())
+      .disabled(Boolean.TRUE.equals(command.getDisabled()))
+      .hash("TODO::")
+      .version(version)
+      .addTransactions(
+          ImmutableSysConfigDeploymentTransaction.builder()
+          .id("1")
+          .addCommands(command)
+          .build())
+      .build();
+    visitedCommands.add(command);
+    
+    return this.current;
+  }
+
+
+  private SysConfigDeployment visitUpdateDeploymentDisabled(UpdateSysConfigDeploymentDisabled command) {
+    this.current = this.current.withUpdated(requireTargetDate(command)).withDisabled(command.getDisabled());
+    visitedCommands.add(command);
+    return this.current;
+  }
+  private SysConfigDeployment visitUpdateDeploymentLiveDate(UpdateSysConfigDeploymentLiveDate command) {
+    this.current = this.current.withUpdated(requireTargetDate(command)).withLiveDate(command.getLiveDate());
+    visitedCommands.add(command);
+    return this.current;
+  }
+  
+  
+  public static Instant requireTargetDate(SysConfigDeploymentCommand command) {
+    final var targetDate = command.getTargetDate();
+    if (targetDate == null) {
+      throw new SysConfigDeploymentVisitorException("targetDate not defined sys config deployment");
+    }
+    return targetDate;
+  }
+
+  
+  public static class NoChangesException extends Exception {
+    private static final long serialVersionUID = -4373837491237504039L;
+  }
+
+  public static class SysConfigDeploymentVisitorException extends RuntimeException {
+    private static final long serialVersionUID = -1385190644836838881L;
+
+    public SysConfigDeploymentVisitorException(String message, Throwable cause) {
+      super(message, cause);
+    }
+
+    public SysConfigDeploymentVisitorException(String message) {
+      super(message);
+    }
+  }
+}
