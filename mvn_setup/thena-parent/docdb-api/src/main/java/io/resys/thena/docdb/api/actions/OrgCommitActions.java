d768a34e621eff25b46907f48d60b0516a67d74b
jocelyn.mutso <jocelyn.mutso@resys.io>
2024-02-26 16:59:44 +0200

    org commit actions

--- mvn_setup/thena-parent/docdb-api/src/main/java/io/resys/thena/docdb/api/actions/OrgCommitActions.java
+++ mvn_setup/thena-parent/docdb-api/src/main/java/io/resys/thena/docdb/api/actions/OrgCommitActions.java
@@ -16,21 +16,44 @@ import io.smallrye.mutiny.Uni;
 
 public interface OrgCommitActions {
   
-  CreateOneUser createOneUser();
+  CreateOneUser createOneUser();  
   CreateOneGroup createOneGroup();
+  CreateOneRole createOneRole();
   
-  ModifyOneGroup createModifyOneGroup();
+  ModifyOneUser modifyOneUser();
+  ModifyOneGroup modifyOneGroup();
+  ModifyOneRole modifyOneRole();
 
-  
-  
+
   interface CreateOneUser {
     CreateOneUser repoId(String repoId);
     CreateOneUser author(String author);
     CreateOneUser message(String message);
+  
+    CreateOneUser userName(String userName);
+    CreateOneUser email(String email);
+    CreateOneUser externalId(@Nullable String externalId);
     
-    // TODO
+    CreateOneUser addUserToGroups(List<String> groupId);
+    CreateOneUser addUserToRoles(List<String> roledId);
     
     Uni<OneUserEnvelope> build();
+  }
+  
+  interface CreateOneGroup {
+    CreateOneGroup repoId(String repoId);
+    CreateOneGroup author(String author);
+    CreateOneGroup message(String message);
+   
+    CreateOneGroup parentId(@Nullable String parentId);
+    CreateOneGroup externalId(@Nullable String externalId);
+    CreateOneGroup groupName(String groupName);
+    CreateOneGroup groupDescription(String groupDescription);
+    
+    CreateOneGroup addUsersToGroup(List<String> userId);
+    CreateOneGroup addRolesToGroup(List<String> roledId);
+
+    Uni<OneGroupEnvelope> build();
   }
   
   
@@ -39,80 +62,99 @@ public interface OrgCommitActions {
     CreateOneRole author(String author);
     CreateOneRole message(String message);
     
-    // TODO
-    
+    CreateOneRole externalId(@Nullable String externalId);
+    CreateOneRole roleName(String roleName);
+    CreateOneRole roleDescription(String roleDescription);
+
+    CreateOneRole addRoleToUsers(List<String> userId);
+    CreateOneRole addRoleToGroups(List<String> groupId);
+        
     Uni<OneRoleEnvelope> build();
   }
   
+
+  interface ModifyOneUser {
+    ModifyOneUser repoId(String repoId);
+    ModifyOneUser author(String author);
+    ModifyOneUser message(String message);
   
-
-  /*
-   * Create doc and doc by: One/Many
-   */
-  interface CreateOneGroup {
-    CreateOneGroup repoId(String repoId);
-    CreateOneGroup parentId(@Nullable String parentId);
-
-    CreateOneGroup author(String author);
-    CreateOneGroup message(String message);
+    ModifyOneUser userId(String userId);
+    ModifyOneUser externalId(@Nullable String externalId);
+    ModifyOneUser userName(String userName);
+    ModifyOneUser email(String email); 
     
-    // TODO
+    ModifyOneUser roles(ModType type, List<String> roles);
+    ModifyOneUser groups(ModType type, List<String> groupId);
     
-
-    Uni<OneGroupEnvelope> build();
+    Uni<OneUserEnvelope> build();
   }
-  
+
+
   interface ModifyOneGroup {
     ModifyOneGroup repoId(String repoId);
-    ModifyOneGroup groupId(String groupId);
-    ModifyOneGroup parentId(@Nullable String parentId);
-    
     ModifyOneGroup author(String author);
     ModifyOneGroup message(String message);
     
-    // TODO
+    ModifyOneGroup groupId(String groupId);
+    ModifyOneGroup parentId(@Nullable String parentId);
+    ModifyOneGroup externalId(@Nullable String externalId);
+    ModifyOneGroup groupName(String groupName);
+    ModifyOneGroup groupDescription(String groupDescription);
     
-    
+    ModifyOneGroup users(ModType type, List<String> userIds);
+    ModifyOneGroup roles(ModType type, List<String> roleIds);
+
     Uni<OneGroupEnvelope> build();
   }
 
   
-  
-  
+  interface ModifyOneRole {
+    ModifyOneRole repoId(String repoId);
+    ModifyOneRole author(String author);
+    ModifyOneRole message(String message);
+    
+    ModifyOneRole roleId(String roleId);
+    ModifyOneRole externalId(@Nullable String externalId);
+    ModifyOneRole roleName(String roleName);
+    ModifyOneRole roleDescription(String roleDescription);
+    
+    ModifyOneRole users(ModType type, List<String> userIds);
+    ModifyOneRole roles(ModType type, List<String> roleIds);
+
+    Uni<OneRoleEnvelope> build();
+  }
   
   
   
   @Value.Immutable
   interface OneRoleEnvelope extends ThenaEnvelope {
     String getRepoId();
-    
-    @Nullable OrgRole getRole();
-    
-    
     CommitResultStatus getStatus();
     List<Message> getMessages();
+
+    @Nullable OrgRole getRole();
   }
   
   
   @Value.Immutable
   interface OneUserEnvelope extends ThenaEnvelope {
     String getRepoId();
-    
-    @Nullable OrgUser getUser();
-    
-    
     CommitResultStatus getStatus();
     List<Message> getMessages();
+    
+    @Nullable OrgUser getUser();
   }
   
   @Value.Immutable
   interface OneGroupEnvelope extends ThenaEnvelope {
     String getRepoId();
-    
-    @Nullable OrgGroup getGroup();
-    
-    
     CommitResultStatus getStatus();
     List<Message> getMessages();
+    
+    @Nullable OrgGroup getGroup();
+  }
+  
+  enum ModType {
+    ADD, OVERRIDE, REMOVE 
   }
 }
