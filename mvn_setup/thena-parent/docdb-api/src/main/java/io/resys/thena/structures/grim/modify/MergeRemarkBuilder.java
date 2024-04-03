9bac4f2df12b538818912f87109a70c4371c2711
Olev Mutso <olev.mutso@resys.io>
2024-04-03 15:26:43 +0300

    mission control state

---
+++ mvn_setup/thena-parent/docdb-api/src/main/java/io/resys/thena/structures/grim/modify/MergeRemarkBuilder.java
@@ -0,0 +1,62 @@
+package io.resys.thena.structures.grim.modify;
+
+import io.resys.thena.api.entities.grim.GrimRemark;
+import io.resys.thena.api.entities.grim.ImmutableGrimRemark;
+import io.resys.thena.api.entities.grim.ThenaGrimContainers.GrimMissionContainer;
+import io.resys.thena.api.entities.grim.ThenaGrimMergeObject.MergeRemark;
+import io.resys.thena.structures.BatchStatus;
+import io.resys.thena.structures.grim.ImmutableGrimBatchForOne;
+import io.resys.thena.structures.grim.commitlog.GrimCommitBuilder;
+import io.resys.thena.support.RepoAssert;
+
+public class MergeRemarkBuilder implements MergeRemark {
+
+  private final GrimCommitBuilder logger;
+  private final ImmutableGrimBatchForOne.Builder batch;
+  private final GrimRemark currentRemark; 
+  private final ImmutableGrimRemark.Builder nextRemark;
+  private boolean built;
+
+  public MergeRemarkBuilder(GrimMissionContainer container, GrimCommitBuilder logger, String missionId, String remarkId) {
+    super();
+    this.logger = logger;
+    this.batch = ImmutableGrimBatchForOne.builder().tenantId(logger.getTenantId()).log("").status(BatchStatus.OK);
+    this.currentRemark = container.getRemarks().get(remarkId);
+    RepoAssert.notNull(currentRemark, () -> "Can't find remark with id: '" + remarkId + "' for mission: '" + missionId + "'!");
+    this.nextRemark = ImmutableGrimRemark.builder().from(currentRemark);
+  }
+  @Override
+  public MergeRemark remarkText(String remarkText) {
+    this.nextRemark.remarkText(remarkText);
+    return this;
+  }
+  @Override
+  public MergeRemark remarkStatus(String remarkStatus) {
+    this.nextRemark.remarkStatus(remarkStatus);
+    return this;
+  }
+  @Override
+  public MergeRemark reporterId(String reporterId) {
+    this.nextRemark.reporterId(reporterId);
+    return this;
+  }
+  @Override
+  public void build() {
+    this.built = true;
+  }
+  public ImmutableGrimBatchForOne close() {
+    RepoAssert.isTrue(built, () -> "you must call MergeRemark.build() to finalize mission MERGE!");
+    
+    var nextRemark = this.nextRemark.build();
+    final var isModified = !nextRemark.equals(currentRemark);
+    if(isModified) {
+      nextRemark = ImmutableGrimRemark.builder()
+          .from(nextRemark)
+          .commitId(this.logger.getCommitId())
+          .build();
+      logger.merge(currentRemark, nextRemark);
+      batch.addUpdateRemarks(nextRemark);
+    }
+    return batch.build();
+  }
+}
