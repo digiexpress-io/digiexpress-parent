afd321be2615f9500370de527a39520e6e7d8431
jocelyn.mutso <jocelyn.mutso@resys.io>
2024-04-10 14:57:14 +0300

    #213 PermissionOverview improvements

--- ts_setup/tasks-ui/src/components-access-mgmt/PermissionsOverview/PermissionItem.tsx
+++ ts_setup/tasks-ui/src/components-access-mgmt/PermissionsOverview/PermissionItem.tsx
@@ -2,35 +2,20 @@
 import React from 'react';
 import { Typography, Grid } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
-import { ImmutableAmStore, Permission } from 'descriptor-access-mgmt';
-import Backend from 'descriptor-backend';
+import { Permission } from 'descriptor-access-mgmt';
 
-export const PermissionItem: React.FC = () => {
-  const backend = Backend.useBackend();
-  const [permissions, setPermissions] = React.useState<Permission[]>();
-
-
-  React.useEffect(() => {
-    new ImmutableAmStore(backend.store).findAllPermissions().then(setPermissions);
-  }, []);
-
-  if (!permissions) {
-    return (<>no permissions defined</>);
-  }
-
+export const PermissionItem: React.FC<{ permission: Permission }> = ({ permission }) => {
   return (<>
-    {permissions.sort((a, b) => a.name.localeCompare(b.name)).map((permission) => <Grid container display='flex' spacing={2} key={permission.id}>
-      <Grid item lg={3}>
-        <Typography><FormattedMessage id='permissions.permission.name' />{": "}{permission.name}</Typography>
-      </Grid>
+    <Grid item lg={3}>
+      <Typography><FormattedMessage id='permissions.permission.name' />{": "}{permission.name}</Typography>
+    </Grid>
 
-      <Grid item lg={7}>
-        <Typography><FormattedMessage id='permissions.permission.description' />{": "}{permission.description}</Typography>
-      </Grid>
+    <Grid item lg={6}>
+      <Typography><FormattedMessage id='permissions.permission.description' />{": "}{permission.description}</Typography>
+    </Grid>
 
-      <Grid item lg={2}>
-        <Typography><FormattedMessage id='permissions.permission.status' />{": "}{permission.status}</Typography>
-      </Grid>
-    </Grid>)}
+    <Grid item lg={3}>
+      <Typography><FormattedMessage id='permissions.permission.status' />{": "}{permission.status}</Typography>
+    </Grid>
   </>)
 }
