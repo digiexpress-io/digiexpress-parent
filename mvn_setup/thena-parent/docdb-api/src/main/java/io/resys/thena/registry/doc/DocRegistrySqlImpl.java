c44c893ac013753520a063c8d72f67b94f82afb8
Olev Mutso <olev.mutso@resys.io>
2024-04-23 19:13:39 +0300

    api clean up

--- mvn_setup/thena-parent/docdb-api/src/main/java/io/resys/thena/registry/doc/DocRegistrySqlImpl.java
+++ mvn_setup/thena-parent/docdb-api/src/main/java/io/resys/thena/registry/doc/DocRegistrySqlImpl.java
@@ -2,8 +2,9 @@ package io.resys.thena.registry.doc;
 
 import io.resys.thena.api.registry.DocRegistry;
 import io.resys.thena.api.registry.doc.DocBranchRegistry;
+import io.resys.thena.api.registry.doc.DocCommandsRegistry;
 import io.resys.thena.api.registry.doc.DocCommitRegistry;
-import io.resys.thena.api.registry.doc.DocLogRegistry;
+import io.resys.thena.api.registry.doc.DocCommitTreeRegistry;
 import io.resys.thena.api.registry.doc.DocMainRegistry;
 import io.resys.thena.datasource.TenantTableNames;
 
@@ -12,7 +13,8 @@ public class DocRegistrySqlImpl implements DocRegistry {
   private final TenantTableNames options;
   private final DocBranchRegistry docBranches;
   private final DocCommitRegistry docCommits;
-  private final DocLogRegistry docLogs;
+  private final DocCommitTreeRegistry docLogs;
+  private final DocCommandsRegistry docCommands;
   private final DocMainRegistry docs;
   
   public DocRegistrySqlImpl(TenantTableNames options) {
@@ -20,7 +22,8 @@ public class DocRegistrySqlImpl implements DocRegistry {
     this.options = options;
     this.docBranches = new DocBranchRegistrySqlImpl(options);
     this.docCommits = new DocCommitRegistrySqlImpl(options);
-    this.docLogs = new DocLogRegistrySqlImpl(options);
+    this.docLogs = new DocCommitTreeRegistrySqlImpl(options);
+    this.docCommands = new DocCommandsRegistrySqlImpl(options);
     this.docs = new DocMainRegistrySqlImpl(options);
   }
   @Override
@@ -32,8 +35,12 @@ public class DocRegistrySqlImpl implements DocRegistry {
     return docCommits;
   }
   @Override
-  public DocLogRegistry docLogs() {
+  public DocCommitTreeRegistry docCommitTrees() {
     return docLogs;
+  }
+  @Override
+  public DocCommandsRegistry docCommands() {
+    return docCommands;
   }
   @Override
   public DocMainRegistry docs() {
