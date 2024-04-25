19bb4b4a5bbf99f965498041c2db5972c53c8c65
Olev Mutso <olev.mutso@resys.io>
2024-04-25 19:37:15 +0300

    api clean up

--- mvn_setup/tasks-parent/sysconfig-client/src/main/java/io/resys/sysconfig/client/spi/visitors/FindAllSysConfigsVisitor.java
+++ mvn_setup/tasks-parent/sysconfig-client/src/main/java/io/resys/sysconfig/client/spi/visitors/FindAllSysConfigsVisitor.java
@@ -6,7 +6,6 @@ import java.util.List;
 import io.resys.sysconfig.client.api.model.Document;
 import io.resys.sysconfig.client.api.model.ImmutableSysConfig;
 import io.resys.sysconfig.client.api.model.SysConfig;
-import io.resys.thena.api.actions.DocQueryActions;
 import io.resys.thena.api.actions.DocQueryActions.DocObjectsQuery;
 import io.resys.thena.api.entities.doc.Doc;
 import io.resys.thena.api.entities.doc.DocBranch;
@@ -15,7 +14,6 @@ import io.resys.thena.api.entities.doc.DocLog;
 import io.resys.thena.api.envelope.DocContainer.DocTenantObjects;
 import io.resys.thena.api.envelope.QueryEnvelope;
 import io.resys.thena.api.envelope.QueryEnvelope.QueryEnvelopeStatus;
-import io.resys.thena.projects.client.spi.store.MainBranch;
 import io.resys.thena.spi.DocStoreException;
 import io.resys.thena.spi.ThenaDocConfig;
 import io.resys.thena.spi.ThenaDocConfig.DocObjectsVisitor;
@@ -24,12 +22,10 @@ import io.smallrye.mutiny.Uni;
 public class FindAllSysConfigsVisitor implements DocObjectsVisitor<List<SysConfig>> {
   @Override
   public Uni<QueryEnvelope<DocTenantObjects>> start(ThenaDocConfig config, DocObjectsQuery builder) {
-    return builder
-        .docType(Document.DocumentType.SYS_CONFIG.name())
-        .branchName(MainBranch.HEAD_NAME);
+    return builder.docType(Document.DocumentType.SYS_CONFIG.name()).findAll();
   }
   @Override
-  public DocQueryActions.DocObjects visitEnvelope(ThenaDocConfig config, QueryEnvelope<DocQueryActions.DocObjects> envelope) {
+  public DocTenantObjects visitEnvelope(ThenaDocConfig config, QueryEnvelope<DocTenantObjects> envelope) {
     if(envelope.getStatus() != QueryEnvelopeStatus.OK) {
       throw DocStoreException.builder("FIND_ALL_SYS_CONFIGS_FAIL").add(config, envelope).build();
     }
@@ -37,7 +33,7 @@ public class FindAllSysConfigsVisitor implements DocObjectsVisitor<List<SysConfi
   }
 
   @Override
-  public List<SysConfig> end(ThenaDocConfig config, DocQueryActions.DocObjects ref) {
+  public List<SysConfig> end(ThenaDocConfig config, DocTenantObjects ref) {
     if(ref == null) {
       return Collections.emptyList();
     }
