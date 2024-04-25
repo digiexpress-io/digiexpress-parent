28b1e1df65b5d4a4affea06782f9e137786f5fcf
Olev Mutso <olev.mutso@resys.io>
2024-04-25 17:16:00 +0300

    api clean up

--- mvn_setup/tasks-parent/crm-client/src/main/java/io/resys/crm/client/spi/visitors/GetActiveCustomerVisitor.java
+++ mvn_setup/tasks-parent/crm-client/src/main/java/io/resys/crm/client/spi/visitors/GetActiveCustomerVisitor.java
@@ -1,22 +1,22 @@
 package io.resys.crm.client.spi.visitors;
 
 import java.util.List;
+import java.util.Map;
 
 import io.resys.crm.client.api.model.Customer;
-import io.resys.crm.client.api.model.ImmutableCustomer;
 import io.resys.crm.client.spi.store.CrmStoreConfig;
 import io.resys.crm.client.spi.store.CrmStoreConfig.DocObjectVisitor;
-import io.resys.thena.api.actions.DocQueryActions;
-import io.resys.thena.api.actions.DocQueryActions.DocObject;
+import io.resys.crm.client.spi.store.CrmStoreException;
 import io.resys.thena.api.actions.DocQueryActions.DocObjectsQuery;
 import io.resys.thena.api.entities.doc.Doc;
 import io.resys.thena.api.entities.doc.DocBranch;
+import io.resys.thena.api.entities.doc.DocCommands;
 import io.resys.thena.api.entities.doc.DocCommit;
-import io.resys.thena.api.entities.doc.DocLog;
+import io.resys.thena.api.entities.doc.DocCommitTree;
+import io.resys.thena.api.envelope.DocContainer.DocObject;
 import io.resys.thena.api.envelope.QueryEnvelope;
 import io.resys.thena.api.envelope.QueryEnvelope.QueryEnvelopeStatus;
-import io.resys.crm.client.spi.store.CrmStoreException;
-import io.resys.crm.client.spi.store.MainBranch;
+import io.smallrye.mutiny.Uni;
 import lombok.RequiredArgsConstructor;
 
 
@@ -25,12 +25,12 @@ public class GetActiveCustomerVisitor implements DocObjectVisitor<Customer>{
   private final String id;
   
   @Override
-  public DocObjectsQuery start(CrmStoreConfig config, DocObjectsQuery query) {
-    return query.matchId(id).branchName(MainBranch.HEAD_NAME);
+  public Uni<QueryEnvelope<DocObject>> start(CrmStoreConfig config, DocObjectsQuery builder) {
+    return builder.get(id);
   }
 
   @Override
-  public DocQueryActions.DocObject visitEnvelope(CrmStoreConfig config, QueryEnvelope<DocQueryActions.DocObject> envelope) {
+  public DocObject visitEnvelope(CrmStoreConfig config, QueryEnvelope<DocObject> envelope) {
     if(envelope.getStatus() != QueryEnvelopeStatus.OK) {
       throw CrmStoreException.builder("GET_CUSTOMER_BY_ID_FAIL")
         .add(config, envelope)
@@ -48,10 +48,12 @@ public class GetActiveCustomerVisitor implements DocObjectVisitor<Customer>{
   }
 
   @Override
-  public Customer end(CrmStoreConfig config, DocQueryActions.DocObject ref) {
-    return ref.accept((Doc doc, DocBranch docBranch, DocCommit commit, List<DocLog> log) -> 
-        docBranch.getValue()
-        .mapTo(ImmutableCustomer.class).withVersion(docBranch.getCommitId())
-        ).iterator().next();
+  public Customer end(CrmStoreConfig config, DocObject ref) {
+    return ref.accept((Doc doc, 
+        DocBranch docBranch, 
+        Map<String, DocCommit> commit, 
+        List<DocCommands> commands,
+        List<DocCommitTree> trees
+    ) -> FindAllCustomersVisitor.mapToCustomer(docBranch, commands)).iterator().next();
   }
 }
