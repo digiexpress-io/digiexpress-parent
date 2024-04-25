2b91cd1a21f31a34411de02b04dc238e7899865f
Olev Mutso <olev.mutso@resys.io>
2024-04-25 16:39:43 +0300

    api clean up

---
+++ mvn_setup/tasks-parent/projects-client/src/main/java/io/resys/thena/projects/client/api/model/TenantConfigTransaction.java
@@ -0,0 +1,16 @@
+package io.resys.thena.projects.client.api.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@Value.Immutable @JsonSerialize(as = ImmutableTenantConfigTransaction.class) @JsonDeserialize(as = ImmutableTenantConfigTransaction.class)
+public
+interface TenantConfigTransaction extends Serializable {
+  String getId();
+  List<TenantConfigCommand> getCommands(); 
+}
