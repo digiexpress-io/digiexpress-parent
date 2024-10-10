c2d5b4b4ba1dd4ea953cc6c726272c8c6ad7022a
Olev Mutso <olev.mutso@resys.io>
2024-10-10 13:38:25 +0300

    eveli build

--- ts_setup/eveli-ide/demo-app/index.tsx
+++ ts_setup/eveli-ide/demo-app/index.tsx
@@ -1,12 +1,23 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { StencilApp } from './stencil-app'
+import { WrenchApp } from './wrench-app'
 
 
+const CreateApp: React.FC = () => {
+  if(process.env.REACT_APP_START_MODE === 'stencil') {
+    return <StencilApp />
+  } else if(process.env.REACT_APP_START_MODE === 'wrench') {
+    return <WrenchApp />
+  }
+
+  return <>unknown app</>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    todo::
+    <CreateApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
