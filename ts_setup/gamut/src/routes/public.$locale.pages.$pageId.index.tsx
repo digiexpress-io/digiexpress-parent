3bd60fa9664bd64e37e502ea7450a912c9cb004a
Olev Mutso <olev.mutso@resys.io>
2024-11-07 13:29:36 +0200

    all-in-one

---
+++ ts_setup/gamut/src/routes/public.$locale.pages.$pageId.index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createFileRoute } from '@tanstack/react-router'
+import { RouterUnsecured } from '../g-routes'
+
+export const Route = createFileRoute('/public/$locale/pages/$pageId/')({
+  component: Component,
+})
+
+function Component() {
+  const { pageId } = Route.useParams()
+  return React.useMemo(() => <ChooseComponent pageId={pageId} />, [pageId])
+}
+
+function ChooseComponent(props: { pageId: string }) {
+  return <RouterUnsecured pageId={props.pageId} />
+}
