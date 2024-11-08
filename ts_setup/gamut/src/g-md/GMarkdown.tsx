db70137f681f736dfa5c390ddca2b212e2bfd305
jocelyn.mutso <jocelyn.mutso@resys.io>
2024-11-08 11:17:20 +0200

    fix react-markdown errors

--- ts_setup/gamut/src/g-md/GMarkdown.tsx
+++ ts_setup/gamut/src/g-md/GMarkdown.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography, useThemeProps } from '@mui/material';
-import ReactMarkdown, { Components } from 'react-markdown'
+import ReactMarkdown from 'react-markdown';
 import { useUtilityClasses, GMarkdownRoot, MUI_NAME } from './useUtilityClasses';
 import { GOverridableComponent } from '../g-override';
 
@@ -18,22 +18,19 @@ export const GMarkdown: React.FC<GMarkdownProps> = (initProps) => {
   });
 
   const classes = useUtilityClasses();
-
-  const components: Components = React.useMemo(() => ({
-    h1: (props) => (<Typography variant={'h1'}>{props.children}</Typography>),
-    h2: (props) => (<Typography variant={'h2'}>{props.children}</Typography>),
-    h3: (props) => (<Typography variant={'h3'}>{props.children}</Typography>),
-    h4: (props) => (<Typography variant={'h4'}>{props.children}</Typography>),
-    h5: (props) => (<Typography variant={'h5'}>{props.children}</Typography>),
-    h6: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
-    p: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
-    ul: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
-  }), []);
-
   const Root = props.component ?? GMarkdownRoot;
   return (
     <Root ownerState={props} className={classes.root}>
-      <ReactMarkdown 
-        children={props.children ?? "no children to render"} components={components} />
+      <ReactMarkdown
+        children={props.children ?? "no children to render"} components={{
+          h1: (props) => (<Typography variant={'h1'}>{props.children}</Typography>),
+          h2: (props) => (<Typography variant={'h2'}>{props.children}</Typography>),
+          h3: (props) => (<Typography variant={'h3'}>{props.children}</Typography>),
+          h4: (props) => (<Typography variant={'h4'}>{props.children}</Typography>),
+          h5: (props) => (<Typography variant={'h5'}>{props.children}</Typography>),
+          h6: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
+          p: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
+          ul: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
+        }} />
     </Root>)
 }
