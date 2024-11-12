e1ab9f0788220b0a35dcd8714f8015c786617f3f
jocelyn.mutso <jocelyn.mutso@resys.io>
2024-11-12 09:04:41 +0200

    cleanup

--- ts_setup/gamut/src/g-form-page/GFormPage.tsx
+++ ts_setup/gamut/src/g-form-page/GFormPage.tsx
@@ -10,7 +10,7 @@ import {
   useThemeInfra,
   GFormPageRoot, GFormPageTitle, GFormPageSubTitle,
   GFormPageBody, GFormPageHeader, GFormPageMenu, GFormPageFooter
-} from './useThemeInfra';
+} from './useUtilityClasses';
 
 
 
