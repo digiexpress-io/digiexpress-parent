86428b972ddf698ef6f65eeb280f6970c1e4bec9
Olev Mutso <olev.mutso@resys.io>
2024-12-20 16:23:56 +0200

    converted sql queries

---
+++ mvn_setup/prod-version/eveli-parent/eveli-client/src/main/java/io/digiexpress/eveli/client/spi/task/AddCustomerCommitViewer.java
@@ -0,0 +1,46 @@
+package io.digiexpress.eveli.client.spi.task;
+
+import io.digiexpress.eveli.client.api.TaskClient;
+import io.resys.thena.api.ThenaClient.GrimStructuredTenant;
+import io.resys.thena.api.actions.GrimCommitActions.ModifyOneMission;
+import io.resys.thena.api.actions.GrimCommitActions.OneMissionEnvelope;
+import io.resys.thena.api.entities.CommitResultStatus;
+import io.resys.thena.api.entities.grim.ThenaGrimMergeObject.MergeMission;
+import io.smallrye.mutiny.Uni;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AddCustomerCommitViewer implements TaskStoreConfig.MergeTaskVisitor<TaskClient.Task> {
+  private final String userId;
+  private final String taskId;
+  
+  public void modify(MergeMission merge) {
+
+    merge
+    // change is viewed by worker who created it
+    .addViewer(viewer -> viewer.userId(userId).usedFor(TaskMapper.VIEWER_CUSTOMER).build())
+    .build();
+  }
+  
+  @Override
+  public ModifyOneMission start(GrimStructuredTenant config, ModifyOneMission builder) {
+    builder.missionId(taskId).modifyMission(merge -> modify(merge));
+    return builder
+        .commitAuthor(userId)
+        .commitMessage("Adding task viewer by: " + AddCustomerCommitViewer.class.getSimpleName());
+  }
+
+  @Override
+  public OneMissionEnvelope visitEnvelope(GrimStructuredTenant config, OneMissionEnvelope envelope) {
+    if(envelope.getStatus() == CommitResultStatus.OK) {
+      return envelope;
+    }
+    throw TaskException.builder("ADD_ONE_CUSTOMER_TASK_VIEWER_FAIL").add(config, envelope).build(); 
+  }
+
+  @Override
+  public Uni<TaskClient.Task> end(GrimStructuredTenant config, OneMissionEnvelope commited) {
+    final var task = TaskMapper.map(commited.getMission(), commited.getAssignments(), commited.getRemarks());
+    return Uni.createFrom().item(task);
+  }
+}
