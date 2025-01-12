c39da3916a7fd511edc8e81423ea2b2b03b3e781
Olev Mutso <olev.mutso@resys.io>
2025-01-12 12:52:38 +0200

    queue

--- mvn_setup/prod-version/thena-parent/mq-client/src/main/java/io/digiexpress/thena/mq/client/spi/ThenaMqConsumerConfigImpl.java
+++ mvn_setup/prod-version/thena-parent/mq-client/src/main/java/io/digiexpress/thena/mq/client/spi/ThenaMqConsumerConfigImpl.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import io.digiexpress.thena.mq.client.api.ThenaMqConsumer;
 import io.digiexpress.thena.mq.client.api.ThenaMqAppConfig;
+import io.digiexpress.thena.mq.client.api.ThenaMqConsumer;
+import io.digiexpress.thena.mq.client.api.entities.Channel;
 import io.digiexpress.thena.mq.client.api.entities.QueueConsumer;
 import io.smallrye.mutiny.tuples.Tuple2;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ThenaMqConsumerConfigImpl implements ThenaMqAppConfig {
   private final String appId;
+  private final Channel channel;
   private final Map<String, Tuple2<QueueConsumer, ThenaMqConsumer>> consumers;
   
   @Override
@@ -21,13 +23,17 @@ public class ThenaMqConsumerConfigImpl implements ThenaMqAppConfig {
     return consumers.get(consumer.getConsumerName()).getItem2();
   }
   
-  public static ThenaMqConsumerConfigImpl from(String appId, List<Tuple2<QueueConsumer, ThenaMqConsumer>> input) {
+  public static ThenaMqConsumerConfigImpl from(String appId, Channel channel, List<Tuple2<QueueConsumer, ThenaMqConsumer>> input) {
     final var consumers = input.stream().collect(Collectors.toMap(e -> e.getItem1().getConsumerName(), e -> e));
-    return new ThenaMqConsumerConfigImpl(appId, consumers);
+    return new ThenaMqConsumerConfigImpl(appId, channel, consumers);
   }
 
   @Override
   public String getAppId() {
     return appId;
+  }
+
+  public Channel getChannel() {
+    return channel;
   }
 }
