cdb35744440e2c8cf09f39c7a7f98aa7329880ff
Olev Mutso <olev.mutso@resys.io>
2025-02-25 11:36:34 +0200

    style cleanup

---
+++ ts_setup/eveli-ide/src/burger/eveli-colors/index.ts
@@ -0,0 +1,26 @@
+
+const grey = '#F3F4F6';
+const blue = 'rgb(80, 72, 229)';
+
+const colors = {
+  grey,
+  blue
+}
+
+/**
+  table: {
+    main: '#F3F4F6', // table header gray
+    dark: '#e8eaed', // table header darker gray
+    light: 'rgba(80, 72, 229, 0.04)',
+    contrastText: '#F3F4F6'
+  },
+
+  uiElements: {
+    main: 'rgb(80, 72, 229)', // primary ui element, blue-purple (button fill, button text, text, checkboxes, etc.)
+    dark: '#F3F4F6', // table header gray
+    light: 'rgba(80, 72, 229, 0.04)', // transparent purple for hover backgrounds, secondary button fill on hover
+    contrastText: 'rgb(80, 72, 229)'
+  },
+ */
+
+export { colors }
