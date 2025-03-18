5dd9751f3ff83ac2f373f6a6ea872b74ed3cffc9
Olev Mutso <olev.mutso@resys.io>
2025-03-18 08:43:30 +0200

    dialob screen

--- ts_setup/eveli-ide/src/routes/secured.$locale.assets.forms.index.tsx
+++ ts_setup/eveli-ide/src/routes/secured.$locale.assets.forms.index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
-import { useLocale } from '@/burger'
+import { useLocale, EveliApp } from '@/burger'
 import { DialobAdminView } from '../frontdesk/views/forms/DialobAdminView';
-
+import { Secondary } from '../frontdesk/Secondary';
+import { Toolbar } from '../frontdesk/Toolbar';
+import { Box } from '@mui/system';
 
 export const Route = createFileRoute('/secured/$locale/assets/forms/')({
   component: Component,
@@ -14,5 +16,10 @@ function Component() {
   const { setLocale } = useLocale();
   React.useLayoutEffect(() => setLocale(locale), [locale])
 
-  return (<DialobAdminView />)
+  return (<EveliApp main={Main} secondary={Secondary} toolbar={Toolbar} />)
+
+}
+
+const Main: React.FC<{}> = () => {
+  return (<Box sx={{ p: 1 }}><DialobAdminView /></Box>)
 }
