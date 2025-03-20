319d590698aed3a6d593684e0f50b4c8d7b5bda2
Vahur Krouverk <vahur.krouverk@resys.io>
2025-03-20 16:23:14 +0200

    #292 changed upload icon name, it is used to find override component for upload

--- ts_setup/eveli-ide/src/frontdesk/views/task/AttachmentTable.tsx
+++ ts_setup/eveli-ide/src/frontdesk/views/task/AttachmentTable.tsx
@@ -3,7 +3,6 @@ import React, { useRef } from 'react';
 import { FormattedDate, FormattedNumber, FormattedTime, useIntl, } from 'react-intl';
 import MaterialTable, { Column, MTableAction  } from '@material-table/core';
 import { Box, Button} from '@mui/material';
-import FileUploadIcon from '@mui/icons-material/FileUpload';
 import DownloadIcon from '@mui/icons-material/Download';
 
 
@@ -151,7 +150,7 @@ export const AttachmentTable:React.FC<Props> = ({ taskId, readonly, attachments,
           }}
           actions={[
             {
-              icon: FileUploadIcon,
+              icon: 'upload',
               isFreeAction: true,
               tooltip: intl.formatMessage({id: 'attachmentButton.addAttachment'}),
               disabled: readonly,
