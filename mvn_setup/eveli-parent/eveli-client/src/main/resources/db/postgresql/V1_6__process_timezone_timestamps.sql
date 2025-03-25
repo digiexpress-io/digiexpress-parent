c263b2ed451cf44bf7cc67764ad9c19897fb003c
Vahur Krouverk <vahur.krouverk@resys.io>
2025-03-25 09:56:46 +0200

    generated comment headers

--- mvn_setup/eveli-parent/eveli-client/src/main/resources/db/postgresql/V1_6__process_timezone_timestamps.sql
+++ mvn_setup/eveli-parent/eveli-client/src/main/resources/db/postgresql/V1_6__process_timezone_timestamps.sql
@@ -1,3 +1,22 @@
+---
+-- #%L
+-- eveli-client
+-- %%
+-- Copyright (C) 2015 - 2025 Copyright 2022 ReSys OÜ
+-- %%
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- #L%
+---
 ALTER TABLE process ALTER created type timestamp with time zone using created at time zone 'UTC';
 ALTER TABLE process ALTER updated type timestamp with time zone using updated at time zone 'UTC';
 ALTER TABLE process ALTER expires_at type timestamp with time zone using expires_at at time zone 'UTC';
