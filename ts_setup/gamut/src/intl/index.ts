86cb3dec24d4991f5e00bf4889dfd9b0ecc52650
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-04-02 13:24:13 +0300

    #318 duplicate forms no longer shown

--- ts_setup/gamut/src/intl/index.ts
+++ ts_setup/gamut/src/intl/index.ts
@@ -2,4 +2,5 @@ import { en } from './en'
 import { fi } from './fi'
 import { sv } from './sv'
 import { my } from './my'
-export default { en, fi, sv, my}
+import { bs } from './bs'
+export default { en, fi, sv, my, bs}
