6afce1e5e27eef6bfc8205cb662bcb393684753b
Olev Mutso <olev.mutso@resys.io>
2025-04-12 16:00:01 +0300

    router

--- ts_setup/gamut/src/router.ts
+++ ts_setup/gamut/src/router.ts
@@ -1,10 +1,1 @@
-import { createRouter } from '@tanstack/react-router'
-
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
-
-// Create a new router instance
-export const router = createRouter({ routeTree })
-
-export { routeTree };
-export type RouterType = typeof router;
+export { routeTree } from './routeTree.gen'
