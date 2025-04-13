393184fb549b8032c4869fcf18ee4ce8221b255b
Olev Mutso <olev.mutso@resys.io>
2025-04-13 14:13:10 +0300

    md overrides

--- ts_setup/gamut/src/g-md/GMarkdown.tsx
+++ ts_setup/gamut/src/g-md/GMarkdown.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography, useThemeProps } from '@mui/material';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown, { Components } from 'react-markdown';
 import { useUtilityClasses, GMarkdownRoot, MUI_NAME } from './useUtilityClasses';
 import { GOverridableComponent } from '../g-override';
 
@@ -8,7 +8,8 @@ import { GOverridableComponent } from '../g-override';
 
 export interface GMarkdownProps {
   children: string | undefined;
-  remarkPlugins?: any[] | undefined
+  remarkPlugins?: any[] | undefined;
+  overrides?: Components
   component?: GOverridableComponent<GMarkdownProps>;
 }
 
@@ -24,16 +25,19 @@ export const GMarkdown: React.FC<GMarkdownProps> = (initProps) => {
     <Root ownerState={props} className={classes.root}>
       <ReactMarkdown
         remarkPlugins={props.remarkPlugins ?? undefined}
-        children={props.children ?? "no children to render"} components={{
-          h1: (props) => (<Typography variant={'h1'}>{props.children}</Typography>),
-          h2: (props) => (<Typography variant={'h2'}>{props.children}</Typography>),
-          h3: (props) => (<Typography variant={'h3'}>{props.children}</Typography>),
-          h4: (props) => (<Typography variant={'h4'}>{props.children}</Typography>),
-          h5: (props) => (<Typography variant={'h5'}>{props.children}</Typography>),
-          h6: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
-          p: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
-          ul: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
-          a: linkRenderer
+        children={props.children ?? "no children to render"} components={
+          
+          {
+            h1: (props) => (<Typography variant={'h1'}>{props.children}</Typography>),
+            h2: (props) => (<Typography variant={'h2'}>{props.children}</Typography>),
+            h3: (props) => (<Typography variant={'h3'}>{props.children}</Typography>),
+            h4: (props) => (<Typography variant={'h4'}>{props.children}</Typography>),
+            h5: (props) => (<Typography variant={'h5'}>{props.children}</Typography>),
+            h6: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
+            p: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
+            ul: (props) => (<Typography variant={'body1'}>{props.children}</Typography>),
+            a: linkRenderer,
+            ...(props.overrides ?? {})
         }} />
     </Root>)
 }
