47408f9eb17fc6bc448ce3b876a1c5f17e412fdd
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-04-15 14:21:24 +0300

    #372 added tanstack table

--- ts_setup/eveli-ide/src/eveli-table/TableTester.tsx
+++ ts_setup/eveli-ide/src/eveli-table/TableTester.tsx
@@ -65,31 +65,27 @@ export const TableTester: React.FC = () => {
 
   return (
     <EveliTable>
-      <thead>
-        {table.getHeaderGroups().map(headerGroup => (
-          <EveliTableHeaderRoot key={headerGroup.id}>
-            {headerGroup.headers.map(header => (
-              <EveliTableHeaderCell1 key={header.id}>
-                {header.isPlaceholder ? null : flexRender(
-                  header.column.columnDef.header,
-                  header.getContext()
-                )}
-              </EveliTableHeaderCell1>
-            ))}
-          </EveliTableHeaderRoot>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map(row => (
-          <EveliTableRowRoot key={row.id}>
-            {row.getVisibleCells().map(cell => (
-              <EveliTableCell key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </EveliTableCell>
-            ))}
-          </EveliTableRowRoot>
-        ))}
-      </tbody>
+      {table.getHeaderGroups().map(headerGroup => (
+        <EveliTableHeaderRoot key={headerGroup.id}>
+          {headerGroup.headers.map(header => (
+            <EveliTableHeaderCell1 key={header.id}>
+              {header.isPlaceholder ? null : flexRender(
+                header.column.columnDef.header,
+                header.getContext()
+              )}
+            </EveliTableHeaderCell1>
+          ))}
+        </EveliTableHeaderRoot>
+      ))}
+      {table.getRowModel().rows.map(row => (
+        <EveliTableRowRoot key={row.id}>
+          {row.getVisibleCells().map(cell => (
+            <EveliTableCell key={cell.id}>
+              {flexRender(cell.column.columnDef.cell, cell.getContext())}
+            </EveliTableCell>
+          ))}
+        </EveliTableRowRoot>
+      ))}
 
       <tfoot>
         {table.getFooterGroups().map(footerGroup => (
