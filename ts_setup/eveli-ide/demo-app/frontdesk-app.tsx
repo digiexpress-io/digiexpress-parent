69a5ea465fa0674ef8690a2915bcbe1794dbf94a
Olev Mutso <olev.mutso@resys.io>
2025-04-23 16:42:30 +0300

    tenant config

--- ts_setup/eveli-ide/demo-app/frontdesk-app.tsx
+++ ts_setup/eveli-ide/demo-app/frontdesk-app.tsx
@@ -84,7 +84,7 @@ export const FrontdeskApp: React.FC = () => {
                 'worker/rest/api/tasks/$taskId/files.POST': fetchOverrideForAttachments,
               }}>
               <ConfigContextProvider logoutUrl={logoutUrl} loginUrl={loginUrl}>
-                <TenantConfigContextProvider>
+                <TenantConfigContextProvider disabled={true}>
                   <IamBackendProvider onExpire={handleExpire}>
                     <RouterProvider router={router} />
                   </IamBackendProvider>
