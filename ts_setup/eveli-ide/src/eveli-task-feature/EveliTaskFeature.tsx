9a24a63940c1a5ef133fcc0320e588e5d494989f
Olev Mutso <olev.mutso@resys.io>
2025-05-05 10:05:26 +0300

    #372 updates to task detail view

---
+++ ts_setup/eveli-ide/src/eveli-task-feature/EveliTaskFeature.tsx
@@ -0,0 +1,26 @@
+import { TaskApi } from '@/api-task';
+import React from 'react';
+import { useTaskFeatures } from './EveliTaskFeatureProvider';
+
+function oneOf(type: TaskApi.TaskFeatureType[]): (input: TaskApi.TaskFeatureType) => boolean {
+  return (input) => type.includes(input);
+}
+
+const EveliTaskFeatureMapping = {
+  'TASK_FEEDBACK': oneOf(['feedback']),
+}
+
+
+export type EveliTaskFeatureType = keyof typeof EveliTaskFeatureMapping;
+
+
+export const EveliTaskFeature: React.FC<{ children: React.ReactNode, id: EveliTaskFeatureType }> = ({ children, id }) => {
+  const { features } = useTaskFeatures();
+  const required = EveliTaskFeatureMapping[id];
+  const isAccessGranted = features.find((permission) => required(permission));
+
+  if (isAccessGranted) {
+    return <>{children}</>
+  }
+  return (<></>)
+}
