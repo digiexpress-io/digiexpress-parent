b9547ba0d472766028037efddc15c6651694662c
Olev Mutso <olev.mutso@resys.io>
2025-05-12 17:21:04 +0300

    annotation issue

---
+++ mvn_setup/eveli-parent/eveli-app/src/main/java/io/digiexpress/eveli/app/ClasspathExternalDeploymentProvider.java
@@ -0,0 +1,52 @@
+package io.digiexpress.eveli.app;
+
+import java.io.InputStream;
+
+/*-
+ * #%L
+ * eveli-app
+ * %%
+ * Copyright (C) 2015 - 2025 Copyright 2022 ReSys OÜ
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.digiexpress.eveli.envir.api.EveliEnvirClient.EveliDeployment;
+import io.digiexpress.eveli.envir.api.ExternalDeploymentProvider;
+import io.smallrye.mutiny.Uni;
+
+
+
+public class ClasspathExternalDeploymentProvider implements ExternalDeploymentProvider {
+
+  private final EveliDeployment deployment;
+  
+  public ClasspathExternalDeploymentProvider(InputStream input, ObjectMapper objectMapper) {
+    try {
+      this.deployment = objectMapper.readValue(input, EveliDeployment.class);
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
+  }
+  
+  
+  @Override
+  public Uni<Optional<EveliDeployment>> getDeployment(boolean emptyBranchBody) {
+    return Uni.createFrom().item(Optional.ofNullable(deployment));
+  }
+}
