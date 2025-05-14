657234125de78f74149eec3b1f0421438dc3af09
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-05-14 13:36:09 +0300

    #406 fixed TaskLink to go to correct task

--- mvn_setup/eveli-parent/eveli-app/src/main/java/io/digiexpress/eveli/app/authentication/AuthenticationConfigFakeUser.java
+++ mvn_setup/eveli-parent/eveli-app/src/main/java/io/digiexpress/eveli/app/authentication/AuthenticationConfigFakeUser.java
@@ -190,7 +190,7 @@ public class AuthenticationConfigFakeUser  {
                     .addressValue("test-street")
                     .build())
                 .build())
-            .type(CustomerType.ANON)
+            .type(CustomerType.AUTH_CUSTOMER)
             .build();
       }
 
