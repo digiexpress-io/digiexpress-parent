6537ddd69534982aa07ec8f44f6cdeb2f557dd85
Olev Mutso <olev.mutso@resys.io>
2025-06-12 10:24:45 +0300

    batch api

---
+++ ts_setup/eveli-ide/src/api-batch/batch-types.ts
@@ -0,0 +1,42 @@
+
+export namespace BatchApi {
+
+}
+
+export declare namespace BatchApi {
+
+  export type BatchStatus = 'ENABLED' | 'DISABLED';
+  export type RuntimeExecutionStatus = 'OK' | 'ERROR';
+  export type RuntimeStatus = 'CREATED' | 'EXECUTING' | 'SKIPPED' | 'COMPLETED' | 'CANCELLED';
+
+
+  export interface Batch {
+    id: string;
+    batchName: string; // unique id, technical name
+    appId: string;
+    externalId: string | undefined; 
+    
+    createdAt: string;
+    createdBy: string;
+    
+    updatedAt: string | string;
+    updatedBy: string | string;
+    status: BatchStatus;
+    
+    comment: string;
+  }
+
+  export interface RuntimeInstance {
+    id: string;
+    batchId: string;
+    
+    createdAt: string;
+    endedAt: string | string;
+    
+    name: string;
+    status: RuntimeStatus;
+    executionStatus: RuntimeExecutionStatus;
+    
+    comment: string;
+  }
+}
---
+++ ts_setup/eveli-ide/src/fetch/worker.rest.api.batches.get.ts
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { createFileFetch } from '@dxs-ts/eveli-fetch';
+import { QueryResult } from '@material-table/core'
+import { Column, Query } from '@material-table/core';
+
+import { TaskApi } from '@/api-task';
+
+import { createMuiTableQueryString } from '@/api-mui-table';
+import { EveliTaskTableContext } from '@/eveli-tasks';
+import { BatchApi } from '@/api-batch';
+
+
+export const Hook = createFileFetch('worker/rest/api/batches.GET')({
+  hook
+})
+
+function hook(props: {}) {
+  const params = Hook.useParams();
+  const { path, contextPath, method, url } = params;
+  const tableContext = React.useContext(EveliTaskTableContext);
+
+
+  return {
+    dashboard: async (): Promise<BatchApi.Batch[]> => {
+      return params.fetch(url({}) )
+        .then(response => response.json());
+    },
+  }
+}
