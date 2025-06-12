0421e977ab79f071725d8dac1e1f1c5f2ef5a1d3
Olev Mutso <olev.mutso@resys.io>
2025-06-12 15:49:11 +0300

    dialob review

---
+++ ts_setup/gamut/src/routes/secured.$locale.forms.$formId.index.tsx
@@ -0,0 +1,31 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { Container } from '@mui/material'
+
+import { GShell } from '../g-shell'
+import { GFormTip } from '../g-form'
+import { WithFormProvider } from '../api-dialob'
+
+export const Route = createFileRoute('/secured/$locale/forms/$formId/')({
+  component: Component,
+})
+function Component() {
+  const { formId } = Route.useParams();
+
+  function handleOnComplete() {
+
+  }
+  
+  return (<>
+    <GShell drawerOpen={false}>
+      <main role='main'>
+        <Container>
+          <WithFormProvider id={formId} executionId={''} variant={''} onAfterComplete={handleOnComplete}>
+            <GFormTip executionId={''} variant={''} onAfterComplete={handleOnComplete} />
+          </WithFormProvider>
+        </Container>
+      </main>
+    </GShell>
+  </>)
+}
+
+
