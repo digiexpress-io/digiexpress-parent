53ddbc90b72ffc37a9909bb229b2444585d2ff2c
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-06-12 12:32:48 +0300

    #373 hardcoded tenantfeatures

--- ts_setup/eveli-ide/src/api-tenant-config/EveliFeatureMapping.ts
+++ ts_setup/eveli-ide/src/api-tenant-config/EveliFeatureMapping.ts
@@ -14,29 +14,29 @@ function notOneOf(required: TenantFeature[]): (given: TenantFeature[]) => boolea
 
 
 export const EveliFeatureMapping = {
-  'LOGIN_BUTTON': notOneOf(['wrench-only']), // wrench is the only application -- everything else is gone
-  'STENCIL_ENABLED': notOneOf(['stencil-disabled']),
-  'WRENCH_ENABLED': notOneOf(['wrench-disabled']),
-  'PUBLICATION_UPDATE': notOneOf(['external-deployment']),
+  'LOGIN_BUTTON': notOneOf(['wrench-only']),                                  // Eveli is reconfigured so as to include only wrench
+  'STENCIL_ENABLED': notOneOf(['stencil-disabled']),                          // Stencil is not available in Eveli 
+  'WRENCH_ENABLED': notOneOf(['wrench-disabled']),                            // Wrench is not available in Eveli
+  'PUBLICATION_UPDATE': notOneOf(['external-deployment']),                    // 
 
-  'STENCIL_LOCALE_FILTER': oneOf(['stencil_locale_filter']),
+  'STENCIL_LOCALE_FILTER': oneOf(['stencil_locale_filter']),                  // Configure visibility of the locale filtering UI component in Stencil
 
-  'DIALOB_ENABLED': notOneOf(['wrench-disabled', 'stencil-disabled']),
+  'DIALOB_ENABLED': notOneOf(['wrench-disabled', 'stencil-disabled']),        
 
-  'WRENCH_RELEASES': notOneOf(['eveli_publication_only']),
+  'WRENCH_RELEASES': notOneOf(['eveli_publication_only']),                    //
   'STENCIL_RELEASES': notOneOf(['eveli_publication_only']),
 
-  'FORM_REVIEW_FLASHY': oneOf(['visual_accommodation']),
+  'FORM_REVIEW_FLASHY': oneOf(['visual_accommodation']),                       //
   'FORM_REVIEW_NORMAL': notOneOf(['visual_accommodation']),
 
-  'QUEUES_ENABLED': notOneOf(['queues-visually-disabled']),
-  'FEEDBACK_ENABLED': notOneOf(['feedback-visually-disabled']),
+  'QUEUES_ENABLED': notOneOf(['queues-visually-disabled']),                   // Queues feature is hidden from Eveli UI
+  'FEEDBACK_ENABLED': notOneOf(['feedback-visually-disabled']),               // Feedback feature is hidden from Eveli UI
 
-  'PROFILE_ENABLED': oneOf(['user_profile']),
+  'PROFILE_ENABLED': oneOf(['user_profile']),                                 // Configure visibility of User profile features in Eveli UI
 
-  'SMART_TABLES': oneOf(['smart_tables']),
+  'SMART_TABLES': oneOf(['smart_tables']),                                    // Configure the usage of (new) improved table component vs original (older) table component
 
-  'BATCHES': oneOf(['batches']),
+  'BATCHES': oneOf(['batches']),                                              // Configure the availability of the Batches feature in Eveli
 }
 
 
