2fa4b4de0b11ed7168b7391835a119e4456afce1
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-06-13 12:07:22 +0300

    #250 Batch table init

--- ts_setup/eveli-ide/src/eveli-batches/EveliBatchesTable.tsx
+++ ts_setup/eveli-ide/src/eveli-batches/EveliBatchesTable.tsx
@@ -1,56 +1,111 @@
 import React from 'react';
-
 import { ColumnDef } from '@tanstack/react-table';
-
 import { useFetch } from '@dxs-ts/eveli-fetch';
-import { TaskApi } from '@/api-task';
-
 import { WithTableStyles } from '@/eveli-table';
-
 import { Box, Typography, IconButton, Tooltip } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { useIntl, FormattedMessage } from 'react-intl';
 
 import { useNavigate } from '@tanstack/react-router';
+import { BatchApi } from '@/api-batch';
+import { BatchHealthBall } from './BatchHealthBall';
 
 
 
 export const EveliBatchesTable: React.FC = () => {
   const intl = useIntl();
-  const { findAll } = useFetch('worker/rest/api/tasks.GET', {})
-  const [data, setData] = React.useState<TaskApi.Task[]>([]);
+  const { findAll } = useFetch('worker/rest/api/batches.GET', {})
+  const [data, setData] = React.useState<BatchApi.Batch[]>([]);
   const navigate = useNavigate();
   
   React.useEffect(() => {
     findAll().then(setData);
   }, []);
 
-  const columns: ColumnDef<TaskApi.Task, any>[] = [
+  const columns: ColumnDef<BatchApi.Batch, any>[] = [
     {
-      header: 'Info',
-      accessorKey: 'additionalInfo',
-      size: 100,
+      header: '',
+      accessorKey: 'health',
+      size: 50,
+      minSize: 50,
+      enableSorting: false,
+      enableResizing: true,
+      cell: () => <BatchHealthBall health="RUNNING_NO_FAILS" />
+    },
+    {
+      header: 'Batch Name',
+      accessorKey: 'batchName',
+      size: 150,
       minSize: 100,
       enableSorting: false,
       enableResizing: true,
       enableColumnFilter: true,
+      meta: {
+        enableSelection: true
+      }
     },
     {
-      header: 'Client',
-      accessorKey: 'clientIdentificator',
+      header: 'Status',
+      accessorKey: 'status',
       filterFn: 'includesString',
       size: 150,
       minSize: 150,
       enableColumnFilter: true,
       enableResizing: true,
-    }
+      meta: {
+        enableSelection: true
+      }
+    },
+    {
+      header: 'Doc type',
+      accessorKey: 'docType',
+      filterFn: 'includesString',
+      size: 150,
+      minSize: 150,
+      enableColumnFilter: true,
+      enableResizing: true,
+      meta: {
+        enableSelection: true
+      }
+    },
+    {
+      header: 'Comment',
+      accessorKey: 'comment',
+      filterFn: 'includesString',
+      size: 250,
+      minSize: 150,
+      enableColumnFilter: true,
+      enableResizing: true,
+    },
+    {
+      header: 'Updated',
+      accessorKey: 'updatedAt',
+      filterFn: 'includesString',
+      size: 150,
+      minSize: 150,
+      enableColumnFilter: true,
+      enableResizing: true,
+    },
+    {
+      header: 'Updated by',
+      accessorKey: 'updatedBy',
+      filterFn: 'includesString',
+      size: 150,
+      minSize: 150,
+      enableColumnFilter: true,
+      enableResizing: true,
+      meta: {
+        enableSelection: true
+      }
+    },
+
   ]
 
   return (
     <Box sx={{ display: 'inline-block' }}>
       <Box display="flex" alignItems="center" mb={2}>
         <Typography variant="h1" sx={{ flexGrow: 1 }}>
-          <FormattedMessage id="batchesView.title" />
+          <FormattedMessage id="batchesView.title" defaultMessage="Batches" />
         </Typography>
 
           <Tooltip title={intl.formatMessage({ id: 'taskButton.addInstance' })}>
