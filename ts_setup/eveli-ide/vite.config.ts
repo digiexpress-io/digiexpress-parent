c6b8c2048f3badb1e62f976b9d6527ecc8f48cd7
Olev Mutso <olev.mutso@resys.io>
2025-08-05 15:32:16 +0300

    Update vite.config.ts

--- ts_setup/eveli-ide/vite.config.ts
+++ ts_setup/eveli-ide/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import serveFrontdesk from './vite.frontdesk.config';
 
 import buildProd from './vite.prod.config';
-
+ 
 // https://vitejs.dev/config/
 export default defineConfig((props) => {
   const { command } = props;
