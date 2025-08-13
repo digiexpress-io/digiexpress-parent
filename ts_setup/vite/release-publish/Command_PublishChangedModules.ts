5e2e04462b93a2bfd89aff777bca20a3da83cf6d
Olev Mutso <olev.mutso@resys.io>
2025-08-13 11:37:05 +0300

    release setup

--- ts_setup/vite/release-publish/Command_PublishChangedModules.ts
+++ ts_setup/vite/release-publish/Command_PublishChangedModules.ts
@@ -40,6 +40,8 @@ export class Command_PublishChangedModules {
         cwd: modulePath,
         timeout: 60000 // 1 minute
       });
+
+      console.log(' Published...', result.stdout);
       
       if (result.success) {
         publishedModules.push(versionEntry.moduleName);
