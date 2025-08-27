4033a426fd0d06dceaa4456c9a88c78eb98de1bb
Olev Mutso <olev.mutso@resys.io>
2025-08-27 11:16:48 +0300

    moved task features

---
+++ ts_setup/modules/task-api/task-feature/TaskFeature.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TaskApi } from '../task-types';
+import { useTaskFeatures } from './TaskFeatureProvider';
+
+
+function oneOf(type: TaskApi.TaskFeatureType[]): (input: TaskApi.TaskFeatureType[]) => boolean {
+  return (input) => {
+    return !!input.find((permission) => type.includes(permission))
+  };
+}
+function notOneOf(type: TaskApi.TaskFeatureType[]): (input: TaskApi.TaskFeatureType[]) => boolean {
+  return (input) => {
+    return !input.find((permission) => type.includes(permission))
+  };
+}
+
+
+const TaskFeatureMapping = {
+  'TASK_FEEDBACK': oneOf(['feedback']),
+  'TASK_TRANSFER': oneOf(['transfer']),
+  'CRM_MESSAGES': notOneOf(['anon']),
+}
+
+export type TaskFeatureType = keyof typeof TaskFeatureMapping;
+export const TaskFeature: React.FC<{ children: React.ReactNode, id: TaskFeatureType }> = ({ children, id }) => {
+  const { features } = useTaskFeatures();
+  const required = TaskFeatureMapping[id];
+  const isEnabled = required(features);
+
+  if (id === 'CRM_MESSAGES') {
+    //console.log(id, { isEnabled, required }, features.map(f => f))
+  }
+  if (isEnabled) {
+    return <>{children}</>
+  }
+  return (<></>)
+}
