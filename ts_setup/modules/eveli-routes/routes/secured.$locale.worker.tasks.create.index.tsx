059e489818054a5006f5cc22210bbf61559c2ad4
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-08-29 11:08:45 +0300

    merged

--- ts_setup/modules/eveli-routes/routes/secured.$locale.worker.tasks.create.index.tsx
+++ ts_setup/modules/eveli-routes/routes/secured.$locale.worker.tasks.create.index.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import { Container } from '@mui/system';
 import { TaskComposer  } from '@dxs-ts/task-composer-v1';
 
+
+//<EveliTaskComposer />
 export const Route = createFileRoute('/secured/$locale/worker/tasks/create/')({
   component: Component,
 })
