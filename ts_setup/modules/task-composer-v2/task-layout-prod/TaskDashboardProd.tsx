ed1f805a51277cda8396fabd1b4659e48f07fb53
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-09-02 12:24:01 +0300

    #488 audit cards

--- ts_setup/modules/task-composer-v2/task-layout-prod/TaskDashboardProd.tsx
+++ ts_setup/modules/task-composer-v2/task-layout-prod/TaskDashboardProd.tsx
@@ -81,7 +81,7 @@ export const TaskDashboardProd: React.FC<{ taskId: string }> = (props) => {
 
   return (
     <TaskDashboardContextProvider taskId={props.taskId}>
-      <CardConfigContextProvider cardTheme='large' initialCardOrder={_variant_debug}>
+      <CardConfigContextProvider cardTheme='large' initialCardOrder={_variant_prod}>
         <TaskDashboardProdInternal />
       </CardConfigContextProvider>
     </TaskDashboardContextProvider>
