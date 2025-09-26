4f945e35969f5ea2a5306785ada74044132535c0
Olev Mutso <olev.mutso@resys.io>
2025-09-26 12:18:26 +0300

    input ref mapping

--- ts_setup/modules/gamut-form/g-input-date/DateAndCalendar.tsx
+++ ts_setup/modules/gamut-form/g-input-date/DateAndCalendar.tsx
@@ -17,38 +17,48 @@ function parseInit(value: string | undefined) {
   return null;
 }
 
-export const DateAndCalendar: React.FC<GInputDateProps> = (props) => {
+interface DelegateInputProps {
+  setExtendedErrors: GInputDateProps['setExtendedErrors'];
+  setValue: (newValue: DateTime | null) => void;
+  value: DateTime | null;
+}
+
+const input = React.forwardRef<any, DelegateInputProps>((props, _ref) => {
   const intl = useIntl();
+  function handleValidity(isError: boolean) {
+    if (!props.setExtendedErrors) {
+      return;
+    }
+    props.setExtendedErrors(isError ? [{
+      id: "invalid-date",
+      code: "invalid-date",
+      description: intl.formatMessage({ id: 'xui.datetime.value.invalid', defaultMessage: 'Invalid date, check format: dd.MM.yyyy' })
+    }] : [])
+  }
+  return (<XuiDatePicker
+    fullWidth
+    onValidity={handleValidity}
+    value={props.value ? props.value.toJSDate() : null}
+    onChange={(d) => {
+      const next = d ? DateTime.fromJSDate(d) : null;
+      props.setValue(next);
+    }}
+  />)
+})
+
+export const DateAndCalendar: React.FC<GInputDateProps> = (props) => {
   const classes = useUtilityClasses(props.id, props.variant);
   const [value, setValue] = React.useState<DateTime | null>(parseInit(props.value));
   const ownerState = { variant: props.variant ?? 'date' };
-
-  const input = React.forwardRef<any, {}>((_itemProps, _ref) => {
-    function handleValidity(isError: boolean) {
-      if (!props.setExtendedErrors) {
-        return;
-      }
-      props.setExtendedErrors(isError ? [{
-        id: "invalid-date",
-        code: "invalid-date",
-        description: intl.formatMessage({ id: 'xui.datetime.value.invalid', defaultMessage: 'Invalid date, check format: dd.MM.yyyy' })
-      }] : [])
-    }
-    return (<XuiDatePicker
-      fullWidth
-      onValidity={handleValidity}
-      value={value ? value.toJSDate() : null}
-      onChange={(d) => {
-        const next = d ? DateTime.fromJSDate(d) : null;
-        setValue(next);
-      }}
-    />)
-  })
+  const { setExtendedErrors } = props;
 
   return (
     <GInputDateInput ownerState={ownerState} className={classes.input}>
       <InputHidden dateTime={value} onChange={props.onChange} id={props.id} />
-      <OutlinedInput fullWidth slots={{ input }} />
+      <OutlinedInput fullWidth 
+        slots={{ input }} slotProps={{ input: 
+          { value, setValue, setExtendedErrors } as any
+        }}/>
     </GInputDateInput>
   );
 };
