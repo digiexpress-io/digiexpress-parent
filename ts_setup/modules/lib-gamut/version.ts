872da7d9730f936605382c2cf37b5c1389886170
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-09-29 07:11:21 +0000

    chore: release versions - @dxs-ts/eveli-ide, @dxs-ts/gamut

--- ts_setup/modules/lib-gamut/version.ts
+++ ts_setup/modules/lib-gamut/version.ts
@@ -1,9 +1,9 @@
 import { VersionInfoApi } from "@dxs-ts/envir-util";
-export const version = '2.0.24';export const build_time = '09/26/2025, 11:38:09';
+export const version = '2.0.25';export const build_time = '09/29/2025, 07:11:21';
 export const renderVersion = () => VersionInfoApi.builder()
           .setLogo('logo_1_great_ones_wisdom')
           .setTheme('red')
-          .setProjectInfo('@dxs-ts/gamut', '2.0.24', '09/26/2025, 11:38:09')
+          .setProjectInfo('@dxs-ts/gamut', '2.0.25', '09/29/2025, 07:11:21')
           .addInternalComponents(['@dxs-ts/envir-util','@dxs-ts/gamut-api','@dxs-ts/gamut-md','@dxs-ts/gamut-primitives','@dxs-ts/gamut-form-review','@dxs-ts/gamut-form','@dxs-ts/gamut-shell','@dxs-ts/gamut-intl','@dxs-ts/gamut-routes','@dxs-ts/gamut-theme','@dxs-ts/xui-datetime'])
           .addExternalComponents(['@mui/icons-material@6.5.0','@mui/material@6.5.0','@mui/system@6.5.0','@mui/utils@6.4.9','@tanstack/react-query@5.83.0','@tanstack/react-router@1.130.2','leaflet-geosearch@4.2.0','luxon@3.7.1','numbro@2.5.0','react@18.3.1','react-intl@6.8.9','react-leaflet@4.2.1','react-markdown@10.1.0','ts-md5@1.3.1'])
           .render();
