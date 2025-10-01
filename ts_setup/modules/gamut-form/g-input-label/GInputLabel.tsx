db7ddeb8c32d733dc811cf558fd6fdc0b5c37be6
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-10-01 09:29:25 +0300

    cleanup

--- ts_setup/modules/gamut-form/g-input-label/GInputLabel.tsx
+++ ts_setup/modules/gamut-form/g-input-label/GInputLabel.tsx
@@ -8,7 +8,7 @@ import { DialobApi } from '@dxs-ts/gamut-api';
 export interface GInputLabelProps {
   id: string;
   labelPosition: DialobApi.ControlLabelPosition,
-  children: string; 
+  children: string;
   braced?: boolean | undefined;
   required: boolean;
   component?: React.ElementType<GInputLabelProps>;
@@ -17,15 +17,18 @@ export interface GInputLabelProps {
 export const GInputLabel: React.FC<GInputLabelProps> = (initProps) => {
   const { classes, props, ownerState } = useThemeInfra(initProps);
   const { labelPosition } = ownerState;
-  
+  const requiredColor = props.required ? 'error.main' : 'text.primary';
 
   return (<GInputLabelRoot className={classes.root} ownerState={ownerState} as={props.component}>
     <>
-      <Typography>{props.children}</Typography>
-      {props.required && <Box display='flex' alignItems='center'>
-        <Box ml={0.5}>*</Box>
-      </Box>}
-      {labelPosition === 'label-left' && <GInputCurlyBracket enabled={ownerState.braced}/> }
+      <Typography sx={{ color: requiredColor }}>{props.children}</Typography>
+      {props.required && (
+        <Box display='flex' alignItems='center'>
+          <Box ml={0.5}><Typography fontSize='15pt' fontWeight='bold' color='error.main'>*</Typography></Box>
+        </Box>
+      )
+      }
+      {labelPosition === 'label-left' && <GInputCurlyBracket enabled={ownerState.braced} />}
     </>
   </GInputLabelRoot>);
 }
