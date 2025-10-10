1fbdeebf833ff415a13fcb31a42e7d30df295ec1
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-10-10 10:38:27 +0300

    #568 Tagomi service composer

--- ts_setup/modules/tagomi-routes/tagomi-service/ServiceComposer.tsx
+++ ts_setup/modules/tagomi-routes/tagomi-service/ServiceComposer.tsx
@@ -10,19 +10,17 @@ import { FormattedMessage } from 'react-intl';
 import { CancelButton } from '@dxs-ts/eveli-primitives';
 import { LocaleLabels } from '../tagomi-locale';
 
-const DUMMY_ID = "none-selected"
-
 
 export const ServiceComposer: React.FC<{ onClose: () => void }> = ({ onClose }) => {
 
-  const { backend, actions, session } = Composer.useComposer();
+  const { backend, actions } = Composer.useComposer();
   const { enqueueSnackbar } = useSnackbar();
   const [serviceName, setServiceName] = React.useState("");
   const [orchestratorName, setOrchestratorName] = React.useState("");
   const [labels, setLabels] = React.useState<TagomiApi.LocaleAndLabel[]>([]);
   const [changeInProgress, setChangeInProgress] = React.useState(false);
 
-  const message = <FormattedMessage id="snack.article.createdMessage" values={{ serviceName }} />
+  const message = <FormattedMessage id="snack.tagomi.services.createdMessage" values={{ serviceName, orchestratorName }} />
 
   const handleCreate = () => {
     const entity: TagomiApi.CreateService = { serviceName, labels, orchestratorName };
@@ -34,22 +32,32 @@ export const ServiceComposer: React.FC<{ onClose: () => void }> = ({ onClose }) 
       actions.handleLoadSite();
     });
   }
- 
+
+
   return (
     <Dialog open={true} onClose={onClose}>
-    <DialogTitle><FormattedMessage id='article.composer.title'/></DialogTitle>
-    <DialogContent>
-      <LocaleLabels
-        onChange={(labels) => { setChangeInProgress(false); setLabels(labels.map(l => ({ locale: l.locale, labelValue: l.value }))); }}
-        onChangeStart={() => setChangeInProgress(true)}
-        selected={labels.map(label => ({ locale: label.locale, value: label.labelValue }))} />
-    </DialogContent>
-    <DialogActions>
-      <CancelButton onClick={onClose} />
-      <Button onClick={handleCreate} disabled={!serviceName}>
-        <FormattedMessage id='article.create'/>
-      </Button>
-    </DialogActions>
-  </Dialog>
+      <DialogTitle><FormattedMessage id='tagomi.service.composer.title' /></DialogTitle>
+      <DialogContent>
+
+        <Burger.TextField label='tagomi.service.composer.serviceName' onChange={setServiceName} value={serviceName} />
+        <Burger.TextField label='tagomi.service.composer.orchestratorName' onChange={setOrchestratorName} value={orchestratorName} />
+        <Box mb={2} />
+
+        <LocaleLabels
+          onChange={(labels) => {
+            setChangeInProgress(false);
+            setLabels(labels.map(l => ({ locale: l.locale, labelValue: l.value })));
+          }}
+          onChangeStart={() => setChangeInProgress(true)}
+          selected={labels.map(label => ({ locale: label.locale, value: label.labelValue }))} />
+      </DialogContent>
+
+      <DialogActions>
+        <CancelButton onClick={onClose} />
+        <Button onClick={handleCreate} disabled={!serviceName || changeInProgress}>
+          <FormattedMessage id='tagomi.service.composer.create' />
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
