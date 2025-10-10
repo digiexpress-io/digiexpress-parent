e58d85c2e6ea05003803ad6b6dbd22f0126591c1
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-10-10 11:10:59 +0300

    #568 Tagomi service composer with workflows

--- ts_setup/modules/tagomi-routes/tagomi-service/ServiceComposer.tsx
+++ ts_setup/modules/tagomi-routes/tagomi-service/ServiceComposer.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Box, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
 import { useSnackbar } from 'notistack';
+import { FormattedMessage } from 'react-intl';
 
 import * as Burger from '@dxs-ts/eveli-primitives';
-
 import { TagomiComposerApi as Composer, TagomiApi } from '@dxs-ts/tagomi-api';
-
-import { FormattedMessage } from 'react-intl';
 import { CancelButton } from '@dxs-ts/eveli-primitives';
 import { LocaleLabels } from '../tagomi-locale';
+import { useFetch } from '@dxs-ts/envir-fetch';
 
 
 export const ServiceComposer: React.FC<{ onClose: () => void }> = ({ onClose }) => {
@@ -21,6 +20,7 @@ export const ServiceComposer: React.FC<{ onClose: () => void }> = ({ onClose }) 
   const [changeInProgress, setChangeInProgress] = React.useState(false);
 
   const message = <FormattedMessage id="snack.tagomi.services.createdMessage" values={{ serviceName, orchestratorName }} />
+  const { flows: allFlows = [] } = useFetch('worker/rest/api/assets/wrench/flow-names.GET', {});
 
   const handleCreate = () => {
     const entity: TagomiApi.CreateService = { serviceName, labels, orchestratorName };
@@ -33,14 +33,17 @@ export const ServiceComposer: React.FC<{ onClose: () => void }> = ({ onClose }) 
     });
   }
 
-
   return (
     <Dialog open={true} onClose={onClose}>
       <DialogTitle><FormattedMessage id='tagomi.service.composer.title' /></DialogTitle>
       <DialogContent>
 
         <Burger.TextField label='tagomi.service.composer.serviceName' onChange={setServiceName} value={serviceName} />
-        <Burger.TextField label='tagomi.service.composer.orchestratorName' onChange={setOrchestratorName} value={orchestratorName} />
+        <Burger.Select label="tagomi.service.composer.orchestratorName" onChange={setOrchestratorName}
+          selected={orchestratorName}
+          items={allFlows.map((flow) => { return { id: flow, value: flow } })}
+        />
+
         <Box mb={2} />
 
         <LocaleLabels
