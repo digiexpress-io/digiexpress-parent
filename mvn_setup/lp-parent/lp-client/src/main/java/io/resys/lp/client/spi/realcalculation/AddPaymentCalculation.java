d3396bc5e07ac053b7b64172a3cadc53bcb36e9e
Olev Mutso <olev.mutso@resys.io>
2025-11-21 15:16:41 +0200

    payment matching

--- mvn_setup/lp-parent/lp-client/src/main/java/io/resys/lp/client/spi/realcalculation/AddPaymentCalculation.java
+++ mvn_setup/lp-parent/lp-client/src/main/java/io/resys/lp/client/spi/realcalculation/AddPaymentCalculation.java
@@ -21,7 +21,6 @@ package io.resys.lp.client.spi.realcalculation;
  */
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 import io.resys.lp.client.api.LpClient.RealCalculation;
 import io.resys.lp.client.api.entities.AnyCalculation;
@@ -29,12 +28,12 @@ import io.resys.lp.client.api.entities.Envelope;
 import io.resys.lp.client.api.entities.Envelope.EnvelopeStatus;
 import io.resys.lp.client.api.entities.ImmutableEnvelope;
 import io.resys.lp.client.api.entities.ImmutableLog;
+import io.resys.lp.client.spi.calc.CalculationFactory;
+import io.resys.thena.api.entities.CommitResultStatus;
 import io.resys.thena.api.envelope.QueryEnvelope;
 import io.resys.thena.contract.client.api.ContractClient;
-import io.resys.thena.contract.client.api.ThenaContractContainers.ContractContainer;
 import io.resys.thena.ledger.client.api.LedgerClient;
-import io.resys.thena.ledger.client.api.ThenaLedgerContainers.LedgerContainer;
-import io.resys.thena.ledger.client.entities.BlackBook;
+import io.resys.thena.ledger.client.api.LedgerCommitActions.OneLedgerEnvelope;
 import io.resys.thena.support.RepoAssert;
 import io.smallrye.mutiny.Uni;
 import lombok.RequiredArgsConstructor;
@@ -44,6 +43,7 @@ import lombok.RequiredArgsConstructor;
 public class AddPaymentCalculation implements RealCalculation {
   private final ContractClient contracts;
   private final LedgerClient ledgers;
+  private final CalculationFactory factory;
   
   private String contractIdOrRefOrEtc;
   private LocalDate startDate;
@@ -83,23 +83,31 @@ public class AddPaymentCalculation implements RealCalculation {
               if(ledger.getObjects() == null) {
                 return notFoundError(contract, "Ledger not found!");
               }
-              return doCalculation(contract, ledger);
+              return factory.builder()
+                  .contract(contract.getObjects())
+                  .ledger(ledger.getObjects())
+                  .startDate(startDate)
+                  .incomingPayments()
+                  .accept()
+                  .onItem().transform(this::ledgerCalculated);
             });
       });
   }
   
-  
-  private Uni<Envelope<AnyCalculation>> doCalculation(
-      QueryEnvelope<ContractContainer> contractContainer, 
-      QueryEnvelope<LedgerContainer> ledgerContainter) {
-  
-    final var ledger = ledgerContainter.getObjects();
-    final var bb = new ArrayList<>(ledger.getBlackBooks());
-    bb.sort(BlackBook.COMPARATOR);
-    
-    
-    return null;
-  }
+  private Envelope<AnyCalculation> ledgerCalculated(OneLedgerEnvelope env) {
+    return ImmutableEnvelope.<AnyCalculation>builder()
+        .status(env.getStatus() == CommitResultStatus.OK ? EnvelopeStatus.OK : EnvelopeStatus.ERROR)
+        .addAllLogs(env.getMessages().stream().map(e -> ImmutableLog.builder()
+            .exception(e.getException())
+            .text(e.getText())
+            .build()).toList())
+        .addLogs(ImmutableLog.builder()
+            .targetId(String.join(",", contractIdOrRefOrEtc))
+            .text("Ledger calculated")
+            .build())
+        .object(null)
+        .build();
+  } 
   
   private Uni<Envelope<AnyCalculation>> notFoundError(QueryEnvelope<?> env, String message) {
     return Uni.createFrom().item(ImmutableEnvelope.<AnyCalculation>builder()
