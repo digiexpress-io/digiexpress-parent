a5d3217b9b084ca4c61cf959dde9519b75125ff9
Olev Mutso <olev.mutso@resys.io>
2025-12-04 19:03:38 +0200

    wrench ast

---
+++ ts_setup/modules/wrench-routes/wrench-flow/autocomplete/Hint_TaskInputMapping.ts
@@ -0,0 +1,82 @@
+import { languages } from 'monaco-editor';
+import { Container } from './Hint';
+import { CompletionItemBuilder } from './CompletionItemBuilder';
+import { HdesApi } from '@dxs-ts/wrench-api';
+
+
+export class Hint_TaskInputMapping {
+  static accept(container: Container): languages.CompletionItem[] {
+    const result: languages.CompletionItem[] = [];
+    
+    if (!container.navDesc.node) {
+      return result;
+    }
+
+    const node = container.navDesc.node;
+    
+    const isInput = node.type === 'FLOW_TASK_ASSET_INPUT';
+    if (!isInput) {
+      return result;
+    }
+
+    const target = container.navDesc.node.parent?.parent?.value;
+    const ref = target?.children["ref"];
+
+    if (!target || !ref) {
+      return result;
+    }
+
+    const linked: HdesApi.Entity<HdesApi.AstBody> | undefined = container.assetsQuery.findOne(ref.value);
+    if (!linked) {
+      return result;
+    }
+
+    const input = node.value;
+    for (const typeDef of container.flow.headers.acceptDefs) {
+      const builder = new CompletionItemBuilder(container.model, container.modelPosition);
+      result.push(builder
+        .id("flow input: " + typeDef.name + " " + typeDef.valueType)
+        .addField(input.keyword, { indent: 10, value: typeDef.name })
+        .build());
+    }
+
+    for (const task of Object.values(container.flow.src.tasks)) {
+      if (task.start > node.start) {
+        continue;
+      }
+
+      const service: HdesApi.AstFlowNode | undefined = task["service"];
+      const decisionTable: HdesApi.AstFlowNode | undefined = task["decisionTable"];
+      const target = decisionTable ? decisionTable : service;
+      
+      if (!target) {
+        continue;
+      }
+      
+      const ref = target.children["ref"];
+      if (!ref) {
+        continue;
+      }
+      
+      let linked: HdesApi.Entity<HdesApi.AstBody> | undefined = container.assetsQuery.findOne(ref.value);
+      if (!linked) {
+        continue;
+      }
+
+      const headers = linked.ast?.headers.returnDefs;
+      if (!headers) {
+        continue;
+      }
+
+      for (const typeDef of headers) {
+        const builder = new CompletionItemBuilder(container.model, container.modelPosition);
+        result.push(builder
+          .id("task output: " + task.id.value + "." + typeDef.name + " " + typeDef.valueType)
+          .addField(input.keyword, { indent: 10, value: task.id.value + '.' + typeDef.name })
+          .build());
+      }
+    }
+    
+    return result;
+  }
+}
