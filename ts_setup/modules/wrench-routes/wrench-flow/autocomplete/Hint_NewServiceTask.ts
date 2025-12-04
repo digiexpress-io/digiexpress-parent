9311abbc39e598a9ec294dc7ddc897755d709215
Olev Mutso <olev.mutso@resys.io>
2025-12-04 18:35:14 +0200

    wrench ast

---
+++ ts_setup/modules/wrench-routes/wrench-flow/autocomplete/Hint_NewServiceTask.ts
@@ -0,0 +1,43 @@
+import { languages } from 'monaco-editor';
+import { Container } from './Hint';
+import { CompletionItemBuilder } from './CompletionItemBuilder';
+import { HintUtils } from './HintUtils';
+
+export class Hint_NewServiceTask {
+  static accept(container: Container): languages.CompletionItem[] {
+    const result: languages.CompletionItem[] = [];
+    const node = container.navDesc.node;
+    if(!node) {
+      return result;
+    }
+
+    const isTasks = node.type === 'FLOW_TASKS';
+    const isTask = node.type === 'FLOW_TASK' && HintUtils.get('id', node.value) && HintUtils.get('then', node.value);
+    
+    if (!(isTasks || isTask)) {
+      return result;
+    }
+
+    // Service task
+    const builder = new CompletionItemBuilder(container.model, container.modelPosition)
+      .id("new service task");
+
+    if(isTasks) {
+      builder
+        .addField("- {name}", { indent: 2 })
+        .addField("id", { indent: 6, value: "{id}" })
+        .addField("then", { indent: 6, value: "next" });
+    }
+
+    builder
+      .addField("{serviceType}", { indent: 6 })
+      .addField("ref", { indent: 8, value: "{ref}" })
+      .addField("collection", { indent: 8, value: "false" })
+      .addField("inputs", { indent: 8 })
+      .guided("service-task");
+
+    result.push(builder.build());
+    
+    return result;
+  }
+}
---
+++ ts_setup/modules/wrench-routes/wrench-flow/autocomplete/Hint_NewSwitchTask.ts
@@ -0,0 +1,46 @@
+import { languages } from 'monaco-editor';
+import { Container } from './Hint';
+import { CompletionItemBuilder } from './CompletionItemBuilder';
+import { HintUtils } from './HintUtils';
+
+export class Hint_NewSwitchTask {
+  static accept(container: Container): languages.CompletionItem[] {
+    const result: languages.CompletionItem[] = [];
+    const node = container.navDesc.node;
+    if(!node) {
+      return result;
+    }
+
+    const isTasks = node.type === 'FLOW_TASKS';
+    const isTask = node.type === 'FLOW_TASK' && 
+      HintUtils.get('id', node.value) && 
+      !HintUtils.get('then', node.value);
+    
+    if (!(isTasks || isTask)) {
+      return result;
+    }
+  
+    // Switch task - only show when task doesn't have 'then' or when in tasks block
+    const builder = new CompletionItemBuilder(container.model, container.modelPosition)
+      .id("new switch task");
+      
+    if(isTasks) {
+      builder
+        .addField("- name", { indent: 2 })
+        .addField("id", { indent: 6, value: "task-id" });
+    }
+
+    builder
+      .addField("switch", { indent: 6 })
+      .addField("- caseName1", { indent: 8 })
+      .addField("when", { indent: 12, value: "when-boolean-expression" })
+      .addField("then", { indent: 12, value: "next-task-id" })
+      .addField("- caseName2", { indent: 8 })
+      .addField("when", { indent: 12, value: "when-boolean-expression" })
+      .addField("then", { indent: 12, value: "next-task-id" });
+      
+    result.push(builder.build());
+  
+    return result;
+  }
+}
