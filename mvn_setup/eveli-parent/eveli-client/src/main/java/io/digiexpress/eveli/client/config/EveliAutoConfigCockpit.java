529008ecd9a6e70644793a957feae39fa6c7018c
Olev Mutso <olev.mutso@resys.io>
2025-12-29 13:31:22 +0200

    cockpit

---
+++ mvn_setup/eveli-parent/eveli-client/src/main/java/io/digiexpress/eveli/client/config/EveliAutoConfigCockpit.java
@@ -0,0 +1,13 @@
+package io.digiexpress.eveli.client.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EveliAutoConfigCockpit {
+
+  
+  // EveliAutoConfigEnvir
+  // EveliAutoConfigContract
+  // EveliAutoConfigAssets
+  // EveliAutoConfig
+}
