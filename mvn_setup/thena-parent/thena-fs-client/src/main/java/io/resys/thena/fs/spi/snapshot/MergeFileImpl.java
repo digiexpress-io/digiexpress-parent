a96f963201f596ab73338978a318423612859863
Olev Mutso <olev.mutso@resys.io>
2026-02-08 14:33:23 +0200

    filesystem

--- mvn_setup/thena-parent/thena-fs-client/src/main/java/io/resys/thena/fs/spi/snapshot/MergeFileImpl.java
+++ mvn_setup/thena-parent/thena-fs-client/src/main/java/io/resys/thena/fs/spi/snapshot/MergeFileImpl.java
@@ -9,7 +9,7 @@ import io.resys.thena.fs.entities.Blob;
 import io.resys.thena.fs.entities.Node;
 import io.resys.thena.fs.entities.Props;
 import io.resys.thena.fs.entities.Ref;
-import io.resys.thena.fs.tables.ImmutablePersistenceUnit;
+import io.resys.thena.support.OidUtils;
 import io.resys.thena.support.RepoAssert;
 import io.vertx.core.json.JsonObject;
 import lombok.RequiredArgsConstructor;
@@ -19,49 +19,107 @@ import lombok.Value;
 @RequiredArgsConstructor
 public class MergeFileImpl implements MergeFile {
   private final Ref lock;
-
+  private final String targetId;
   
+  private String filePath;
+  private String fileName;
+  private JsonObject fileValue;
+  private BiConsumer<Props, PropsBuilder> fileProps;
+  private boolean validated = false;
 
   @Override
   public MergeFile fileValue(JsonObject blob) {
-    // TODO Auto-generated method stub
-    return null;
+    this.fileValue = blob;
+    return this;
   }
-
 
   @Override
   public MergeFile fileName(String name) {
-    // TODO Auto-generated method stub
-    return null;
+    this.fileName = name;
+    return this;
   }
-
 
   @Override
   public MergeFile filePath(String path) {
-    // TODO Auto-generated method stub
-    return null;
+    this.filePath = path;
+    return this;
   }
-
 
   @Override
   public MergeFile fileProps(BiConsumer<Props, PropsBuilder> props) {
-    // TODO Auto-generated method stub
-    return null;
+    this.fileProps = props;
+    return this;
   }
   
   @Override
   public void build() {
-    final var previousNode = nodesById.get(command.docId());
-    RepoAssert.notNull(previousNode, () -> "Can't find file to merge by id: '" + command.docId() + "'!");
+    if(filePath != null) {
+      validateFilePath(this.filePath);
+    }
     
-    final var previousProps = Optional.ofNullable(propsByNodeId.get(previousNode.getId()));
-    final var previousBlob = previousNode.getBlobId().map(blobsById::get).orElseThrow();
+    validateFileName(this.fileName);
+    validateFileValue(this.fileValue);
     
-    
+    this.validated = true;
   }
 
   public MergeFileResult close() {
+    RepoAssert.isTrue(validated, () -> "build() method must be called before close()");
     
+    final var prevNode = lock.getTransitives().getNodesById().get(targetId);
+    final var prevProps = prevNode.getPropsId().map(lock.getTransitives().getPropsById()::get);
+    final var prevBlob = prevNode.getPropsId().map(lock.getTransitives().getBlobsById()::get);
+    
+    // TODO: Load existing node from lock.getTransitives().getTree() by path/name
+    // For now using placeholders
+    final var nodeId = OidUtils.genUUID(); // TODO: use existing node ID
+    final var existingProps = (Props) null; // TODO: load existing props
+    
+    // Create new blob if fileValue provided
+    final var blob = Optional.ofNullable(this.fileValue)
+        .map(value -> {
+          // TODO: determine fileType from existing blob or default
+          return Blob.newInstance(value, "application/json").build();
+        })
+        .orElse(null); // TODO: use existing blob
+    
+    // Merge props if provided
+    final var props = Optional.ofNullable(this.fileProps)
+        .map(p -> {
+          final var builder = new PropsBuilderImpl(Optional.of(lock));
+          p.accept(existingProps, builder);
+          return builder.close().getProps();
+        })
+        .orElse(existingProps);
+    
+    final var blobId = Optional.ofNullable(blob).map(Blob::getId);
+    final var propsId = Optional.ofNullable(props).map(Props::getId);
+    
+    final var path = Optional.ofNullable(this.filePath);
+    final var name = this.fileName;
+    final var node = Node.newInstance(path.orElse(""), nodeId, name, blobId, propsId).build();
+    
+    return new MergeFileResult(node, blob, props);
+  }
+  
+  private void validateFilePath(String path) {
+    RepoAssert.notNull(path, () -> "filePath is required");
+    RepoAssert.isTrue(!path.trim().isEmpty(), () -> "filePath cannot be empty");
+    RepoAssert.isTrue(path.matches("^[a-zA-Z0-9/_-]+$"), () -> "filePath contains invalid characters, only a-z, A-Z, 0-9, /, _, - allowed");
+    RepoAssert.isTrue(!path.contains("//"), () -> "filePath cannot contain double slashes");
+    RepoAssert.isTrue(!path.endsWith("/"), () -> "filePath cannot end with slash");
+  }
+  
+  private void validateFileName(String name) {
+    RepoAssert.notNull(name, () -> "fileName is required");
+    RepoAssert.isTrue(!name.trim().isEmpty(), () -> "fileName cannot be empty");
+    RepoAssert.isTrue(name.matches("^[a-zA-Z0-9_.-]+$"), () -> "fileName contains invalid characters, only a-z, A-Z, 0-9, _, ., - allowed");
+    RepoAssert.isTrue(!name.contains("/"), () -> "fileName cannot contain slashes");
+  }
+  
+  private void validateFileValue(JsonObject value) {
+    RepoAssert.notNull(value, () -> "fileValue is required");
+    RepoAssert.isTrue(!value.isEmpty(), () -> "fileValue cannot be empty");
   }
   
   @Value
--- mvn_setup/thena-parent/thena-fs-client/src/main/java/io/resys/thena/fs/spi/snapshot/PropsBuilderImpl.java
+++ mvn_setup/thena-parent/thena-fs-client/src/main/java/io/resys/thena/fs/spi/snapshot/PropsBuilderImpl.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import io.resys.thena.fs.api.commits.CommitBuilder.PropsBuilder;
 import io.resys.thena.fs.entities.Props;
 import io.resys.thena.fs.entities.Ref;
+import io.resys.thena.support.RepoAssert;
 import io.vertx.core.json.JsonObject;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
@@ -13,38 +14,65 @@ import lombok.Value;
 @RequiredArgsConstructor
 public class PropsBuilderImpl implements PropsBuilder {
   private final Optional<Ref> lock;
+  
+  private JsonObject propsLabels;
+  private JsonObject propsComments;
+  private JsonObject propsPermissions;
+  private JsonObject propsFlags;
+  private boolean validated = false;
 
   @Override
   public PropsBuilder propsLabels(JsonObject labels) {
-    // TODO Auto-generated method stub
-    return null;
+    this.propsLabels = labels;
+    return this;
   }
 
   @Override
   public PropsBuilder propsComments(JsonObject comments) {
-    // TODO Auto-generated method stub
-    return null;
+    this.propsComments = comments;
+    return this;
   }
 
   @Override
   public PropsBuilder propsPermissions(JsonObject permissions) {
-    // TODO Auto-generated method stub
-    return null;
+    this.propsPermissions = permissions;
+    return this;
   }
 
   @Override
   public PropsBuilder propsFlags(JsonObject flags) {
-    // TODO Auto-generated method stub
-    return null;
+    this.propsFlags = flags;
+    return this;
   }
 
   @Override
   public void build() {
-    // TODO Auto-generated method stub
-    
+    validateProps();
+    this.validated = true;
   }
+  
   public NewPropsResult close() {
+    RepoAssert.isTrue(validated, () -> "build() method must be called before close()");
     
+    final var props = Props.newInstance(
+        this.propsLabels,
+        this.propsComments, 
+        this.propsPermissions,
+        this.propsFlags
+    ).build();
+    
+    return new NewPropsResult(props);
+  }
+  
+  private void validateProps() {
+    RepoAssert.isTrue(
+      !(
+        propsComments == null || 
+        propsLabels == null || 
+        propsFlags == null ||
+        propsPermissions == null
+      ), 
+      () -> "props cannot be all nulls if provided");
   }
   
   @Value
