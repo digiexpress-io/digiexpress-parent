1f74d7de7c5bc38a079ddbed1d1c475a5ce39059
Olev Mutso <olev.mutso@resys.io>
2026-02-09 11:56:34 +0200

    filesystem

--- mvn_setup/thena-parent/thena-fs-client/src/main/java/io/resys/thena/fs/spi/snapshot/PropsBuilderImpl.java
+++ mvn_setup/thena-parent/thena-fs-client/src/main/java/io/resys/thena/fs/spi/snapshot/PropsBuilderImpl.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import io.resys.thena.fs.api.commits.CommitBuilder.PropsBuilder;
 import io.resys.thena.fs.entities.Props;
-import io.resys.thena.fs.entities.Ref;
 import io.resys.thena.support.RepoAssert;
 import io.vertx.core.json.JsonObject;
 import lombok.RequiredArgsConstructor;
@@ -50,10 +49,10 @@ public class PropsBuilderImpl implements PropsBuilder {
   public void build() {
     RepoAssert.isTrue(
         !(
-          propsComments == null || 
-          propsLabels == null || 
-          propsFlags == null ||
-          propsPermissions == null
+          propsComments.isNewValueSet() || 
+          propsLabels.isNewValueSet() || 
+          propsFlags.isNewValueSet() ||
+          propsPermissions.isNewValueSet()
         ), 
         () -> "props cannot be all nulls if provided");
     this.validated = true;
@@ -63,10 +62,10 @@ public class PropsBuilderImpl implements PropsBuilder {
     RepoAssert.isTrue(validated, () -> "build() method must be called before close()");
     
     final var props = Props.newInstance(
-        this.propsLabels,
-        this.propsComments, 
-        this.propsPermissions,
-        this.propsFlags
+        this.propsLabels.orElse(lock.map(Props::getPropsLabels).orElse(null)),
+        this.propsComments.orElse(lock.map(Props::getPropsComments).orElse(null)),
+        this.propsPermissions.orElse(lock.map(Props::getPropsPermissions).orElse(null)),
+        this.propsFlags.orElse(lock.map(Props::getPropsFlags).orElse(null))
     ).build();
     
     return new NewPropsResult(props);
