9e04a95d2c53353dec56f45dd4a2caf240cde3b1
Olev Mutso <olev.mutso@resys.io>
2026-02-20 10:20:36 +0200

    mig

---
+++ mvn_setup/eveli-parent/eveli-migration/src/main/java/io/digiexpress/eveli/mig/v6/assets/AssetMerger.java
@@ -0,0 +1,24 @@
+package io.digiexpress.eveli.mig.v6.assets;
+
+import io.digiexpress.eveli.mig.v6.baseline.OldGit;
+import lombok.Value;
+
+public class AssetMerger {
+
+  
+  
+  void visitStencil(OldGit.OldGitObjects stencil) {
+    
+  }
+  
+  
+  AssetMergerResult close() {
+    return new AssetMergerResult();
+  }
+  
+  
+  @Value
+  public static class AssetMergerResult {
+    
+  }
+}
