8d871654ca636aa36e79b76394a050a2bf268141
Olev Mutso <olev.mutso@resys.io>
2026-02-24 11:08:39 +0200

    thena compiler

---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/authoring/articletemplate/ModifyArticleTemplate.java
@@ -0,0 +1,30 @@
+package io.resys.limaone.authoring.articletemplate;
+
+import java.util.function.Consumer;
+
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import io.resys.limaone.model.ArticleTemplate;
+import io.resys.limaone.model.Model;
+import io.smallrye.mutiny.Uni;
+
+public interface ModifyArticleTemplate {
+  
+  ModifyArticleTemplate props(ModifyArticleTemplateProps props);
+  ModifyArticleTemplate props(Consumer<ImmutableModifyArticleTemplateProps.Builder> props);
+  
+  Uni<Model<ArticleTemplate>> build();
+  
+  
+  @Value.Immutable @JsonSerialize(as = ImmutableModifyArticleTemplateProps.class) @JsonDeserialize(as = ImmutableModifyArticleTemplateProps.class)
+  interface ModifyArticleTemplateProps {
+    String getTemplateId();
+    String getName();
+    String getDescription();
+    String getContent();
+    String getType();
+  }
+}
---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/authoring/articleworkflow/ModifyArticleWorkflow.java
@@ -0,0 +1,46 @@
+package io.resys.limaone.authoring.articleworkflow;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import io.resys.limaone.model.ArticleWorkflow;
+import io.resys.limaone.model.LocaleLabel;
+import io.resys.limaone.model.Model;
+import io.smallrye.mutiny.Uni;
+import jakarta.annotation.Nullable;
+
+public interface ModifyArticleWorkflow {
+  
+  ModifyArticleWorkflow props(ModifyArticleWorkflowProps props);
+  ModifyArticleWorkflow props(Consumer<ImmutableModifyArticleWorkflowProps.Builder> props);
+  
+  Uni<Model<ArticleWorkflow>> build();
+  
+  
+  @Value.Immutable @JsonSerialize(as = ImmutableModifyArticleWorkflowProps.class) @JsonDeserialize(as = ImmutableModifyArticleWorkflowProps.class)
+  interface ModifyArticleWorkflowProps {
+    String getWorkflowId();
+    String getValue();
+    
+    @Nullable String getFormId();
+    @Nullable String getFormName();
+    @Nullable String getFormTag();
+    @Nullable String getFlowName();
+    @Nullable List<LocaleLabel> getLabels();
+    @Nullable List<String> getArticles();
+    
+    @Nullable Boolean getDisabled();
+    @Nullable Boolean getDevMode();
+    @Nullable Boolean getAnon();
+    @Nullable Boolean getAssignable();
+    
+    @Nullable OffsetDateTime getStartDate();
+    @Nullable OffsetDateTime getEndDate();
+  }
+}
