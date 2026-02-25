00b1bb2e6d7f89640bf37e99e83d376984deb1ef
Olev Mutso <olev.mutso@resys.io>
2026-02-25 19:10:17 +0200

    thena compiler

---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/spi/program/article/SiteVisitor.java
@@ -0,0 +1,97 @@
+package io.resys.limaone.spi.program.article;
+
+import java.time.LocalDateTime;
+
+/*-
+ * #%L
+ * stencil-static-content
+ * %%
+ * Copyright (C) 2021 Copyright 2021 ReSys OÜ
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.List;
+import java.util.Map;
+
+import jakarta.annotation.Nullable;
+
+import org.immutables.value.Value;
+
+import io.thestencil.client.api.Markdowns.Heading;
+import io.thestencil.client.api.Markdowns.ImageTag;
+import io.thestencil.client.api.MigrationBuilder.LocalizedSite;
+
+public interface SiteVisitor {
+  void visitTopicData(TopicData topic);
+  void visitLinkData(LinkData link);
+  void visitImageData(ImageData image);
+  void visitTopicNameData(TopicNameData names);
+  SiteVisitorOutput visit(String imageUrl);
+  
+  @Value.Immutable
+  interface SiteVisitorOutput {
+    List<Message> getMessage();
+    List<LocalizedSite> getSites();
+  }
+  
+  @Value.Immutable
+  interface Message {
+    String getText();
+    @Nullable
+    Object getObject();
+  }
+
+  @Value.Immutable
+  interface TopicData {
+    String getPath();
+    String getLocale();
+    String getValue();
+    Boolean getAuth();
+    List<Heading> getHeadings();
+    List<ImageTag> getImages();
+  }
+
+  @Value.Immutable
+  interface TopicNameData {
+    String getPath();
+    Map<String, String> getLocale();
+  }
+
+  @Value.Immutable
+  interface LinkData {
+    String getId();
+    String getPath();
+    String getName();
+    String getType();
+    String getValue();
+    String getLocale();
+    Boolean getGlobal();
+    Boolean getAnon();
+    Boolean getAssignable();
+    Boolean getWorkflow();
+    @Nullable LocalDateTime getStartDate();
+    @Nullable LocalDateTime getEndDate();
+    @Nullable String getFormId();
+    @Nullable String getFormName();
+    @Nullable String getFormTag();
+    @Nullable String getFlowName();
+  }
+
+  @Value.Immutable
+  interface ImageData {
+    String getPath();
+    byte[] getValue();
+  }
+}
