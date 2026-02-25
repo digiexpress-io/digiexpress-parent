625e02a2c2f4868c8b8e92d350d5279768057a3a
Olev Mutso <olev.mutso@resys.io>
2026-02-25 18:09:50 +0200

    thena compiler

--- mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/program/FlowProgram.java
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/program/FlowProgram.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.annotation.Nullable;
 
 
-@Value.Immutable
+
 public interface FlowProgram extends Program {
   
   String getStartStepId();
