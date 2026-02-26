4cc1aa6f5d3fae27c18d45ab1f1360f8ea91857b
Olev Mutso <olev.mutso@resys.io>
2026-02-26 09:02:57 +0200

    thena compiler

--- mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/program/Compiler.java
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/program/Compiler.java
@@ -1,10 +1,37 @@
 package io.resys.limaone.program;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
 
-public interface Compiler {
+import io.resys.limaone.model.Model.ModelWorld;
 
+public interface Compiler {  
+  
+  Bundle compile(ModelWorld world);
+  
+  interface Bundle {
+    String getId();
+    String getName();
+    String getExternalId();
+    
+    OffsetDateTime getCreated();
+    OffsetDateTime getStartDate();
+    OffsetDateTime getEndDate();
+
+    BundleQuery<DialobProgram> queryDialob();
+    BundleQuery<ArticleProgram> queryArticles();
+    BundleQuery<FlowTaskProgram> queryFlowTasks();
+    BundleQuery<FlowProgram> queryFlows();
+    BundleQuery<DecisionProgram> queryDecisions();
+  }
   
   
-  
-  ProgramEnvir compile();
+  interface BundleQuery<T> {
+    BundleQuery<T> name(String name);
+    BundleQuery<T> externalId(String externalId);
+    BundleQuery<T> id(String id);
+    
+    Optional<T> findOne();
+    T getOne();
+  }
 }
