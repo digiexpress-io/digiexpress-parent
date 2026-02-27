9f17c44497af952ee3bed7f8b04cdcdfb4e25045
Olev Mutso <olev.mutso@resys.io>
2026-02-27 16:10:51 +0200

    thena compiler

--- mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/spi/compiler/flowtask/Compiler_FlowTask.java
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/spi/compiler/flowtask/Compiler_FlowTask.java
@@ -1,8 +1,9 @@
-package io.resys.limaone.spi.compiler;
+package io.resys.limaone.spi.compiler.flowtask;
 
 import io.resys.limaone.model.FlowTask;
 import io.resys.limaone.model.Model;
 import io.resys.limaone.model.Model.ModelWorld;
+import io.resys.limaone.spi.compiler.CompilableUnit;
 import lombok.RequiredArgsConstructor;
 
 
