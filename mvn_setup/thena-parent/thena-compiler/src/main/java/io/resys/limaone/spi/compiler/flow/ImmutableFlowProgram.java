c57e0eb7ee6449bad77c3686f1d12077f809a675
Olev Mutso <olev.mutso@resys.io>
2026-02-28 08:05:58 +0200

    thena compiler

---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/spi/compiler/flow/ImmutableFlowProgram.java
@@ -0,0 +1,123 @@
+package io.resys.limaone.spi.compiler.flow;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import io.resys.limaone.ast.Flow_AST;
+import io.resys.limaone.ast.Flow_AST.FlowInputNode;
+import io.resys.limaone.model.Model.BodyType;
+import io.resys.limaone.model.Parameter;
+import io.resys.limaone.model.Parameter.Direction;
+import io.resys.limaone.model.Parameter.ValueType;
+import io.resys.limaone.program.FlowProgram;
+import io.resys.limaone.program.Runtime;
+import io.resys.limaone.spi.ast.flow.AstFlowNodesFactory;
+import io.resys.limaone.spi.parameter.Parameter_Factory;
+import io.resys.limaone.spi.program.ProgramException;
+
+public class ImmutableFlowProgram implements FlowProgram {
+
+  private static final long serialVersionUID = -4209510801206880302L;
+  private final Flow_AST ast;
+  
+  private final String startStepId;
+  private final Map<String, FlowProgramStep> steps;
+  
+  private final ProgramStatus status; 
+  private final List<Parameter> headers;
+  private final List<ProgramMessage> errors;
+  private final List<ProgramAssociation> associations;
+  
+  
+  public ImmutableFlowProgram(
+      Flow_AST ast, 
+      String startStepId,
+      Map<String, FlowProgramStep> steps,
+      ProgramStatus status,
+      List<ProgramMessage> errors,
+      List<ProgramAssociation> associations) {
+    super();
+    this.ast = ast;
+    this.status = status;
+    this.startStepId = startStepId;
+    this.steps = Collections.unmodifiableMap(steps);
+    this.errors = Collections.unmodifiableList(errors);
+    this.associations = Collections.unmodifiableList(associations);
+    this.headers = Collections.unmodifiableList(getHeaders(ast));
+  }
+  
+  private List<Parameter> getHeaders(Flow_AST ast) {
+    final Map<String, FlowInputNode> inputs = ast.getRoot().getInputs();
+
+    int index = 0;
+    final List<Parameter> result = new ArrayList<>();
+    for (Map.Entry<String, FlowInputNode> entry : inputs.entrySet()) {
+      if (entry.getValue().getType() == null) {
+        continue;
+      }
+      try {
+        ValueType valueType = ValueType.valueOf(entry.getValue().getType().getValue());
+        boolean required = AstFlowNodesFactory.getBooleanValue(entry.getValue().getRequired());
+        result.add(Parameter_Factory.newParam()
+            .id(entry.getValue().getStart() + "")
+            .order(index++)
+            .name(entry.getKey()).valueType(valueType).direction(Direction.IN).required(required)
+            .values(AstFlowNodesFactory.getStringValue(entry.getValue().getDebugValue()))
+            .build());
+        
+      } catch (Exception e) {
+        final String msg = String.format("Failed to convert data type from: %s, error: %s", entry.getValue().getType().getValue(), e.getMessage());
+        throw new ProgramException(msg, e);
+      }
+    }
+    return result;
+  }
+  
+  @Override
+  public String getId() {
+    return ast.getId();
+  }
+
+  @Override
+  public BodyType getType() {
+    return ast.getBodyType();
+  }
+
+  @Override
+  public ProgramStatus getStatus() {
+    return status;
+  }
+
+  @Override
+  public List<Parameter> getHeaders() {
+    return headers;
+  }
+  @Override
+  public List<ProgramMessage> getErrors() {
+    return errors;
+  }
+
+  @Override
+  public List<ProgramAssociation> getAssociations() {
+    return associations;
+  }
+
+  @Override
+  public String getStartStepId() {
+    return startStepId;
+  }
+
+  @Override
+  public Map<String, FlowProgramStep> getSteps() {
+    return steps;
+  }
+
+  @Override
+  public FlowExecutor run(ProgramInput input, Runtime runtime) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/spi/compiler/flowtask/ImmutableFlowTaskProgram.java
@@ -0,0 +1,100 @@
+package io.resys.limaone.spi.compiler.flowtask;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.collect.Streams;
+
+import io.resys.limaone.ast.FlowTask_AST;
+import io.resys.limaone.model.FlowTask.FlowTaskExecutable;
+import io.resys.limaone.model.FlowTask.FlowTaskPropType;
+import io.resys.limaone.model.Model.BodyType;
+import io.resys.limaone.model.Parameter;
+import io.resys.limaone.program.FlowTaskProgram;
+import io.resys.limaone.program.Runtime;
+
+public class ImmutableFlowTaskProgram implements FlowTaskProgram {
+  private static final long serialVersionUID = 5909985495850322300L;
+
+  private final FlowTask_AST ast;
+  
+  private final FlowTaskExecutable executable;
+  
+  private final ProgramStatus status; 
+  private final List<Parameter> headers;
+  private final List<ProgramMessage> errors;
+  private final List<ProgramAssociation> associations;
+  
+  public ImmutableFlowTaskProgram(
+      FlowTask_AST ast,
+      FlowTaskExecutable executable,
+      ProgramStatus status,
+      List<ProgramMessage> errors,
+      List<ProgramAssociation> associations) {
+    super();
+    this.ast = ast;
+    this.executable = executable;
+    this.status = status;
+    this.errors = Collections.unmodifiableList(errors);
+    this.associations = Collections.unmodifiableList(associations);
+    this.headers = Streams
+        .concat(ast.getHeaders().getAcceptDefs().stream(), ast.getHeaders().getReturnDefs().stream())
+        .toList();
+  }
+
+  @Override
+  public String getId() {
+    return ast.getId();
+  }
+
+  @Override
+  public BodyType getType() {
+    return ast.getBodyType();
+  }
+
+  @Override
+  public ProgramStatus getStatus() {
+    return status;
+  }
+
+  @Override
+  public List<Parameter> getHeaders() {
+    return headers;
+  }
+  @Override
+  public List<ProgramMessage> getErrors() {
+    return errors;
+  }
+
+  @Override
+  public List<ProgramAssociation> getAssociations() {
+    return associations;
+  }
+
+  @Override
+  public FlowTaskPropType getExecutorType() {
+    return ast.getExecutorType();
+  }
+
+  @Override
+  public FlowTaskExecutable getBean() {
+    return executable;
+  }
+
+  @Override
+  public Parameter getTypeDef0() {
+    return ast.getTypeDef0();
+  }
+
+  @Override
+  public Parameter getTypeDef1() {
+    return ast.getTypeDef1();
+  }
+
+  @Override
+  public FlowTaskExecutor run(ProgramInput input, Runtime runtime) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
