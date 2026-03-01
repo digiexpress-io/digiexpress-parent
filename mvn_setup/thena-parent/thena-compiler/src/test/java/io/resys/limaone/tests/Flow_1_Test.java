67094cb37af3eb5943ed99c45db356017154b69c
Olev Mutso <olev.mutso@resys.io>
2026-03-01 08:53:52 +0200

    thena compiler

---
+++ mvn_setup/thena-parent/thena-compiler/src/test/java/io/resys/limaone/tests/Flow_1_Test.java
@@ -0,0 +1,165 @@
+package io.resys.limaone.tests;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.resys.hdes.client.api.ast.ImmutableAstCommand;
+import io.resys.hdes.client.api.ast.AstCommand.AstCommandValue;
+
+/*-
+ * #%L
+ * wrench-component-assets-Dt
+ * %%
+ * Copyright (C) 2016 - 2017 Copyright 2016 ReSys OÜ
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import io.resys.hdes.client.api.programs.DecisionProgram.DecisionResult;
+import io.resys.hdes.client.spi.util.FileUtils;
+import io.resys.hdes.client.test.config.TestUtils;
+import io.vertx.core.json.JsonObject;
+
+public class Flow_1_Test {
+
+  @Test
+  public void firstMatch() throws IOException {
+    
+    final var envir = TestUtils.client.envir().tagName("simulate")
+        .addCommand().id("dt-1").decision(FileUtils.toString(getClass(), "decision/match-first.json")).build()
+        .addCommand().id("dt-2").decision(FileUtils.toString(getClass(), "decision/match-last.json")).build()
+        .addCommand().id("optional-dt-combination").flow(flow(
+"""
+id: optional-dt-combination
+inputs:
+  clientType:
+    required: true
+    type: STRING
+  test:
+    required: true
+    type: BOOLEAN
+  passed:
+    required: true
+    type: BOOLEAN
+  lang:
+    required: true
+    type: STRING
+  age:
+    required: true
+    type: INTEGER
+    
+tasks:
+  - split flow:
+    id: splitFlow
+    switch: 
+      - private:
+          when: clientType == \"private\"
+          then: first-match
+      - company:
+          when: clientType == \"company\"
+          then: last-match
+
+  - first-match:
+    id: first-match
+    then: format_text
+    decisionTable:
+      ref: first-row-null-result
+      collection: false
+      inputs:
+        test: test
+        passed: passed
+        language: lan
+          
+  - last-match:
+    id: last-match
+    then: format_text
+    decisionTable:
+      ref: last-row-null-result
+      collection: false
+      inputs:
+        age: age
+        lan: lang
+          
+  - format text:
+    id: format_text
+    then: end
+    returns:
+        inputs:
+          ageLiabilityTxt: last-match.ageText
+          knowlidgeLiabilityTxt: first-match.educationText
+"""))
+        .build()
+        .build(); 
+  
+    
+    
+    {
+  
+      Map<String, Serializable> values = new HashMap<>();
+      values.put("test", true);
+      values.put("passed", false);
+      values.put("language", "fi");
+  
+      DecisionResult result = TestUtils.client.executor(envir).inputMap(values).decision("first-row-null-result").andGetBody();
+  
+      Assertions.assertEquals(1, result.getMatches().size());
+      Assertions.assertEquals(0, result.getMatches().get(0).getOrder());
+      Assertions.assertEquals(0, result.getMatches().get(0).getReturns().size());
+    }
+    
+    {
+
+      Map<String, Serializable> values = new HashMap<>();
+      values.put("age", 50);
+      values.put("lan", "es");
+  
+      DecisionResult result = TestUtils.client.executor(envir).inputMap(values).decision("last-row-null-result").andGetBody();
+  
+      Assertions.assertEquals(1, result.getMatches().size());
+      Assertions.assertEquals(4, result.getMatches().get(0).getOrder());
+      Assertions.assertEquals(0, result.getMatches().get(0).getReturns().size());
+    }
+    
+    
+    {            
+      Map<String, Serializable> values = new HashMap<>();
+      values.put("clientType", "company");
+      values.put("test", true);
+      values.put("passed", false);
+      values.put("age", 50);
+      values.put("lang", "es");  
+
+      
+      final var result = TestUtils.client.executor(envir).inputMap(values).flow("optional-dt-combination")
+          .andGetBody();
+    
+      Assertions.assertEquals("{}", JsonObject.mapFrom(result.getReturns()).encode());
+    }
+  }
+  
+  public String flow(String value) throws JsonProcessingException {
+    return TestUtils.objectMapper.writeValueAsString(Arrays.asList(ImmutableAstCommand.builder()
+      .type(AstCommandValue.SET_BODY)
+      .value(value)
+      .build()));
+  }
+}
---
+++ mvn_setup/thena-parent/thena-compiler/src/test/java/io/resys/limaone/tests/Flow_3_Test.java
@@ -0,0 +1,178 @@
+package io.resys.limaone.tests;
+
+/*-
+ * #%L
+ * hdes-client-api
+ * %%
+ * Copyright (C) 2015 - 2025 Copyright 2022 ReSys OÜ
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.resys.hdes.client.api.ast.AstCommand.AstCommandValue;
+import io.resys.hdes.client.api.ast.ImmutableAstCommand;
+import io.resys.hdes.client.api.programs.FlowProgram.FlowExecutionStatus;
+import io.resys.hdes.client.test.config.TestUtils;
+
+
+public class Flow_3_Test {
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void runAll() throws IOException {
+    
+    final var envir = TestUtils.client.envir().tagName("FlowWith2DtTest")
+        .addCommand().id("events_dt").decision(events_dt()).build()
+        .addCommand().id("queues_dt").decision(queues_dt()).build()
+        .addCommand().id("flow with 2 DT").flow(flow(
+"""
+id: flow with 2 DT
+inputs:
+  path:
+    required: true
+    type: STRING
+  operation:
+    required: true
+    type: STRING
+
+tasks:
+  - match all events:
+      id: "task_events"
+      then: "task_event_queues"
+      decisionTable:
+        ref: events_dt
+        collection: true
+        inputs:
+          path: path
+          op: operation
+        
+  - map events to queues:
+      id: "task_event_queues"
+      then: "format_result"
+      decisionTable:
+        ref: queues_dt
+        collection: true
+        inputs:
+          event: task_events.event
+          
+  - format result:
+      id: "format_result"
+      then: "end"
+      returns:
+        collection: true
+        inputs:
+          event: task_event_queues._event
+          queue: task_event_queues.queue
+          queue2: task_event_queues.queue
+          queue3: task_event_queues.queue
+"""))
+        .build()
+        .build();
+    
+    
+    final var result = TestUtils.client.executor(envir)
+      .inputMap(Map.of(
+        "path", "",
+        "operation", ""
+      ))
+      .flow("flow with 2 DT").andGetBody();
+
+    
+    
+    Assertions.assertEquals(result.getStatus(), FlowExecutionStatus.COMPLETED);
+    
+    final List<Map<String, Object>> values = (List<Map<String, Object>>) result.getReturns().get("");
+    Assertions.assertEquals(1, values.size());
+    Assertions.assertEquals("{queue2=sms, queue3=sms, event=TASK_UPDATED, queue=sms}", values.get(0).toString());
+  }
+
+
+  public String flow(String value) throws JsonProcessingException {
+    return TestUtils.objectMapper.writeValueAsString(Arrays.asList(ImmutableAstCommand.builder()
+    .type(AstCommandValue.SET_BODY)
+    .value(value)
+    .build()));
+  }
+  
+  public String events_dt() throws JsonProcessingException {
+    
+    return TestUtils.objectMapper.writeValueAsString(Arrays.asList(
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_NAME).value("events_dt").build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HIT_POLICY).value("ALL").build(),
+        
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_HEADER_IN).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_REF).id("0").value("path").build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_TYPE).id("0").value("STRING").build(),
+        
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_HEADER_IN).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_REF).id("1").value("op").build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_TYPE).id("1").value("STRING").build(),
+
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_HEADER_OUT).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_REF).id("2").value("event").build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_TYPE).id("2").value("STRING").build(),
+        
+        
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_ROW).build(),//3
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("4").value(null).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("5").value(null).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("6").value("TASK_CREATED").build(),
+        
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_ROW).build(),//7
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("8").value(null).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("9").value(null).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("10").value("TASK_UPDATED").build()
+    ));
+  }
+  
+  
+  public String queues_dt() throws JsonProcessingException {
+    return TestUtils.objectMapper.writeValueAsString(Arrays.asList(
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_NAME).value("queues_dt").build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HIT_POLICY).value("ALL").build(),
+        
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_HEADER_IN).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_REF).id("0").value("event").build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_TYPE).id("0").value("STRING").build(),
+        
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_HEADER_OUT).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_REF).id("1").value("queue").build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_HEADER_TYPE).id("1").value("STRING").build(),
+        
+        
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_ROW).build(),//2
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("3").value(in("TASK_CREATED")).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("4").value("sms").build(),
+        
+        ImmutableAstCommand.builder().type(AstCommandValue.ADD_ROW).build(),//5
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("6").value(in("TASK_UPDATED")).build(),
+        ImmutableAstCommand.builder().type(AstCommandValue.SET_CELL_VALUE).id("7").value("sms").build()
+    ));
+  }
+  
+  private String in(String exp) {
+    return "in[\"" + exp + "\"]";
+  }
+}
