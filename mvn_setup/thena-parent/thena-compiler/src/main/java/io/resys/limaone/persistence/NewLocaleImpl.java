0d95fdd53af7a259c6240d2a525f1ba49837d251
Olev Mutso <olev.mutso@resys.io>
2026-03-06 17:09:01 +0200

    thena compiler

---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/persistence/NewLocaleImpl.java
@@ -0,0 +1,64 @@
+package io.resys.limaone.persistence;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import io.resys.limaone.authoring.ImmutableNewLocaleProps;
+import io.resys.limaone.authoring.ImmutableNewLocaleProps.Builder;
+import io.resys.limaone.authoring.NewLocale;
+import io.resys.limaone.model.ImmutableLocale;
+import io.resys.limaone.model.Locale;
+import io.resys.limaone.model.Model;
+import io.resys.limaone.model.Model.BodyType;
+import io.resys.limaone.model.Model.ModelWorld;
+import io.smallrye.mutiny.Uni;
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+public class NewLocaleImpl implements NewLocale {
+  private final WorldPersistence persistence;
+  private NewLocaleProps props;
+  
+  @Override
+  public NewLocale props(NewLocaleProps props) {
+    this.props = props;
+    return this;
+  }
+
+  @Override
+  public NewLocale props(Consumer<Builder> props) {
+    final var builder = ImmutableNewLocaleProps.builder();
+    props.accept(builder);
+    return props(builder.build());
+  }
+
+  @Override
+  public Uni<Model<Locale>> build() {
+    return persistence.worldBuilder()
+      .docs(BodyType.LOCALE)
+      .lock().build(nextWorld -> {
+        final var body = internalBuild(nextWorld.getCurrentWorld());
+        return nextWorld.newModel(body);
+      });
+  }
+  
+  private Locale internalBuild(ModelWorld world) {
+    Objects.requireNonNull(props, () -> "props must be defined");
+
+    final var locale = ImmutableLocale.builder()
+        .value(props.getLocale())
+        .enabled(true);
+ 
+    final var duplicate = world.getLocales().values().stream()
+        .filter(p -> p.getBody().getValue().equals(props.getLocale()))
+        .findFirst();
+    
+    if(duplicate.isPresent()) {
+      final var msg = "Locale: '" + props.getLocale() + "' already exists!";
+      throw new AuthoringException(props, msg);
+    }
+    
+    return locale.build();
+  }
+}
