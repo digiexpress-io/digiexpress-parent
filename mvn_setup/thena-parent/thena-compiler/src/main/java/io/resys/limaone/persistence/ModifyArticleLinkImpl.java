143b7ecb255222f460118346a1c9d8d6995e0540
Olev Mutso <olev.mutso@resys.io>
2026-03-10 07:58:04 +0200

    thena compiler

---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/persistence/ModifyArticleLinkImpl.java
@@ -0,0 +1,106 @@
+package io.resys.limaone.persistence;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import io.resys.limaone.authoring.ImmutableModifyArticleLinkProps;
+import io.resys.limaone.authoring.ImmutableModifyArticleLinkProps.Builder;
+import io.resys.limaone.authoring.ModifyArticleLink;
+import io.resys.limaone.model.ArticleLink;
+import io.resys.limaone.model.ImmutableArticleLink;
+import io.resys.limaone.model.ImmutableLocaleLabel;
+import io.resys.limaone.model.Model;
+import io.resys.limaone.model.Model.BodyType;
+import io.resys.limaone.model.Model.ModelWorld;
+import io.resys.limaone.persistence.AuthoringImpl.AuthoringConfig;
+import io.resys.limaone.persistence.WorldPersistence.NextWorld;
+import io.smallrye.mutiny.Uni;
+
+
+public class ModifyArticleLinkImpl extends AuthoringTemplate<ModifyArticleLinkImpl, Model<ArticleLink>> implements ModifyArticleLink {
+
+  private ModifyArticleLinkProps props;
+
+  public ModifyArticleLinkImpl(AuthoringConfig config) {
+    super(config);
+  }
+  
+  @Override
+  public ModifyArticleLink props(ModifyArticleLinkProps props) {
+    this.props = props;
+    return this;
+  }
+
+  @Override
+  public ModifyArticleLink props(Consumer<Builder> props) {
+    final var builder = ImmutableModifyArticleLinkProps.builder();
+    props.accept(builder);
+    return props(builder.build());
+  }
+
+  @Override
+  public Uni<Model<ArticleLink>> build() {
+    return config.getPersistence().worldBuilder()
+      .createdAt(getCreatedAt())
+      .author(getAuthor())
+      .docs(BodyType.LOCALE, BodyType.ARTICLE, BodyType.ARTICLE_LINK)
+      .build(nextWorld -> {
+        final var body = internalBuild(nextWorld);
+        return nextWorld.mergeModel(props.getLinkId(), body.getValue(), body);
+      });
+  }
+  
+  private ArticleLink internalBuild(NextWorld nextWorld) {
+    Objects.requireNonNull(props, () -> "props must be defined");
+    final ModelWorld world = nextWorld.getCurrentWorld();
+    
+    final var start = world.getArticleLinks().get(props.getLinkId());
+    if(start == null) {
+      throw new AuthoringException(props, "Article link with id: '" + props.getLinkId() + "' not found!");
+    }
+    
+    final var link = ImmutableArticleLink.builder()
+      .from(start.getBody())
+      .devMode(props.getDevMode())
+      .contentType(props.getType())
+      .value(props.getValue());
+    
+    // Handle articles if provided
+    if(props.getArticles() != null) {
+      final var articles = new ArrayList<String>();
+      for(final var articleRef : props.getArticles()) {
+        final var article = world.findOneArticle(articleRef);
+
+        if(article.isEmpty()) {
+          throw new AuthoringException(props, 
+              "Article with id: '" + articleRef + "' does not exist in: '" + String.join(",", world.getArticles().keySet()) + "'!");          
+        }
+        articles.add(article.get().getId());
+      }
+      link.articles(articles);
+    }
+    
+    // Handle labels if provided
+    if(props.getLabels() != null) {
+      link.labels(Collections.emptyList());
+      for(final var label : props.getLabels()) {
+        final var localeRef = label.getLocale();
+        final var locale = world.findOneLocale(localeRef);
+            
+        link.addLabels(ImmutableLocaleLabel.builder()
+            .locale(locale.map(e -> e.getId()).orElse(localeRef))
+            .labelValue(label.getLabelValue())
+            .build());
+
+        if(locale.isEmpty()) {
+          throw new AuthoringException(props, 
+              "Locale with id: '" + label.getLocale() + "' does not exist in: '" + String.join(",", world.getLocales().keySet()) + "'!");          
+        }
+      }
+    }
+    
+    return link.build();
+  }
+}
---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/persistence/ModifyArticleTemplateImpl.java
@@ -0,0 +1,79 @@
+package io.resys.limaone.persistence;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import io.resys.limaone.authoring.ImmutableModifyArticleTemplateProps;
+import io.resys.limaone.authoring.ImmutableModifyArticleTemplateProps.Builder;
+import io.resys.limaone.authoring.ModifyArticleTemplate;
+import io.resys.limaone.model.ArticleTemplate;
+import io.resys.limaone.model.ImmutableArticleTemplate;
+import io.resys.limaone.model.Model;
+import io.resys.limaone.model.Model.BodyType;
+import io.resys.limaone.model.Model.ModelWorld;
+import io.resys.limaone.persistence.AuthoringImpl.AuthoringConfig;
+import io.resys.limaone.persistence.WorldPersistence.NextWorld;
+import io.smallrye.mutiny.Uni;
+
+
+public class ModifyArticleTemplateImpl extends AuthoringTemplate<ModifyArticleTemplateImpl, Model<ArticleTemplate>> implements ModifyArticleTemplate {
+
+  private ModifyArticleTemplateProps props;
+
+  public ModifyArticleTemplateImpl(AuthoringConfig config) {
+    super(config);
+  }
+  
+  @Override
+  public ModifyArticleTemplate props(ModifyArticleTemplateProps props) {
+    this.props = props;
+    return this;
+  }
+
+  @Override
+  public ModifyArticleTemplate props(Consumer<Builder> props) {
+    final var builder = ImmutableModifyArticleTemplateProps.builder();
+    props.accept(builder);
+    return props(builder.build());
+  }
+
+  @Override
+  public Uni<Model<ArticleTemplate>> build() {
+    return config.getPersistence().worldBuilder()
+      .createdAt(getCreatedAt())
+      .author(getAuthor())
+      .docs(BodyType.ARTICLE_TEMPLATE)
+      .build(nextWorld -> {
+        final var body = internalBuild(nextWorld);
+        return nextWorld.mergeModel(props.getTemplateId(), body.getName(), body);
+      });
+  }
+  
+  private ArticleTemplate internalBuild(NextWorld nextWorld) {
+    Objects.requireNonNull(props, () -> "props must be defined");
+    final ModelWorld world = nextWorld.getCurrentWorld();
+    
+    final var start = world.getArticleTemplates().get(props.getTemplateId());
+    if(start == null) {
+      throw new AuthoringException(props, "Article template with id: '" + props.getTemplateId() + "' not found!");
+    }
+    
+    // Check for duplicate name
+    final var duplicate = world.getArticleTemplates().values().stream()
+        .filter(p -> !p.getId().equals(props.getTemplateId()))
+        .filter(p -> p.getBody().getName().equals(props.getName()))
+        .findFirst();
+
+    if(duplicate.isPresent()) {
+      throw new AuthoringException(props, "Template: '" + props.getName() + "' already exists!");
+    }
+
+    return ImmutableArticleTemplate.builder()
+      .from(start.getBody())
+      .name(props.getName())
+      .description(props.getDescription())
+      .content(props.getContent())
+      .type(props.getType())
+      .build();
+  }
+}
