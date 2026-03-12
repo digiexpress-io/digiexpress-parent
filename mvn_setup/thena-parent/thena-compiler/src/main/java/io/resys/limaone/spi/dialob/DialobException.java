a31a2047aac32b5dec9082c682138bc2fbb700b3
Olev Mutso <olev.mutso@resys.io>
2026-03-12 11:33:52 +0200

    thena compiler

---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/spi/dialob/DialobException.java
@@ -0,0 +1,14 @@
+package io.resys.limaone.spi.dialob;
+
+
+
+public class DialobException extends RuntimeException {
+  private static final long serialVersionUID = 4905500192836989583L;
+
+  public DialobException(String message, Throwable cause) {
+    super(message, cause);
+  }
+  public DialobException(String message) {
+    super(message);
+  }
+}
