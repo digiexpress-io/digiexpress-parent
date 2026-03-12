44e7e9ddcf36f0b8e2d1981ea1fa21b2b425ec0a
Olev Mutso <olev.mutso@resys.io>
2026-03-12 18:38:05 +0200

    migration support

---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/spi/ast/DialobFormParserImpl.java
@@ -0,0 +1,62 @@
+package io.resys.limaone.spi.ast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+import io.resys.limaone.ast.AST_Parser.DialobFormParser;
+import io.resys.limaone.ast.DialobForm_AST;
+import io.resys.limaone.ast.ImmutableDialobForm_AST;
+import io.resys.limaone.ast.ImmutableHeaders_AST;
+import io.resys.limaone.model.DialobForm;
+import io.resys.limaone.model.Model;
+import io.resys.limaone.model.Model.BodyType;
+import io.resys.limaone.model.ModelError;
+import io.resys.limaone.spi.LocalCache;
+import io.resys.limaone.spi.LocalCache.DialobForm_AST_CacheKey;
+import io.resys.limaone.spi.ast.AST_ParserImpl.AST_ParserProps;
+import lombok.RequiredArgsConstructor;
+
+
+
+@RequiredArgsConstructor
+public class DialobFormParserImpl implements DialobFormParser {
+  private final AST_ParserProps props;
+  private Model<DialobForm> model;
+  
+  @Override
+  public DialobFormParserImpl model(Model<DialobForm> workflow) {
+    this.model = Objects.requireNonNull(workflow, () -> "workflow must be defined");
+    return this;
+  }
+
+  @Override
+  public DialobForm_AST parse() {
+    Objects.requireNonNull(model, () -> "workflow must be defined");
+
+    final var hash = model.getBodyHash();
+    final var cacheKey = new DialobForm_AST_CacheKey(hash);
+    
+    final Function<DialobForm_AST_CacheKey, DialobForm_AST> mappingFunction = (k) -> createAst(model);
+    final var ast = LocalCache.computeIfAbsent(cacheKey, mappingFunction);
+    
+    return ast;
+  }
+
+  private DialobForm_AST createAst(Model<DialobForm> model) {
+    final List<ModelError> errors = new ArrayList<>();
+    return ImmutableDialobForm_AST.builder()
+      .addAllErrors(errors)
+      .bodyType(BodyType.DIALOB_FORM)
+      .headers(ImmutableHeaders_AST.builder().build())
+      .name(getFormDep(model.getBody().getFormName(), model.getBody().getFormTagName()))
+      .hash(model.getBodyHash())
+      .form(model.getBody().getForm())
+      .build();
+  }
+
+  public static String getFormDep(String formName, String tagName) {
+    return formName + "/" + tagName;
+  }
+}
---
+++ mvn_setup/thena-parent/thena-compiler/src/main/java/io/resys/limaone/spi/program/DialobProgramImpl.java
@@ -0,0 +1,76 @@
+package io.resys.limaone.spi.program;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import io.resys.limaone.ast.DialobForm_AST;
+import io.resys.limaone.model.DialobForm;
+import io.resys.limaone.model.Model;
+import io.resys.limaone.model.Model.BodyType;
+import io.resys.limaone.model.ModelError;
+import io.resys.limaone.model.Parameter;
+import io.resys.limaone.program.DialobProgram;
+import io.resys.limaone.program.ProgramInput;
+import io.resys.limaone.program.Runtime;
+import io.resys.limaone.program.WorkflowProgram;
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+public class DialobProgramImpl implements DialobProgram {
+  private final Model<DialobForm> target;
+  private final DialobForm_AST ast;
+  private final ProgramStatus status;
+  private final List<ModelError> errors;
+  private final List<ProgramAssociation> associations;
+  
+  @Override
+  public String getId() {
+    return target.getId();
+  }
+
+  @Override
+  public String getName() {
+    return ast.getName();
+  }
+
+  @Override
+  public BodyType getType() {
+    return BodyType.DIALOB_FORM;
+  }
+  
+  @Override
+  public ProgramStatus getStatus() {
+    return status;
+  }
+
+  @Override
+  public List<Parameter> getHeaders() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<ModelError> getErrors() {
+    return errors;
+  }
+
+  @Override
+  public List<ProgramAssociation> getAssociations() {
+    return associations;
+  }
+
+  @Override
+  public DialobExecutor run(ProgramInput input, Runtime runtime) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public DialobExecutor run(Map<String, Serializable> input) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
