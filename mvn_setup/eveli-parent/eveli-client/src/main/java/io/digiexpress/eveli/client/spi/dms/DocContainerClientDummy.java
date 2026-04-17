f51046f3cc33ebf1d2769c470006386e4d6b0dbc
Olev Mutso <olev.mutso@resys.io>
2026-04-17 12:15:34 +0300

    transfer task template

--- mvn_setup/eveli-parent/eveli-client/src/main/java/io/digiexpress/eveli/client/spi/dms/DocContainerClientDummy.java
+++ mvn_setup/eveli-parent/eveli-client/src/main/java/io/digiexpress/eveli/client/spi/dms/DocContainerClientDummy.java
@@ -24,38 +24,45 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import io.digiexpress.eveli.client.api.TaskClient.Task;
+import io.digiexpress.eveli.client.api.TaskClient.TaskStatus;
 import io.digiexpress.eveli.client.spi.dms.DocContainer.Doc;
+import io.digiexpress.eveli.client.spi.task.TaskMapper;
+import io.resys.thena.api.entities.grim.ThenaGrimMergeObject.MergeMission;
 import io.smallrye.mutiny.Uni;
+import io.vertx.core.json.JsonObject;
 
 public class DocContainerClientDummy implements DocContainerClient {
 
   @Override
-  public DocContainerBuilder containerBuilder() {
-    return new DocContainerBuilder() {
+  public CreateDoc createDoc() {
+    return new CreateDoc() {
       private final List<Doc> docsToMerge = new ArrayList<>();
+      @SuppressWarnings("unused")
+      private Task task;
       
       @Override
-      public DocContainerBuilder title(String userGivenId) {
+      public CreateDoc title(String userGivenId) {
         return this;
       }
       @Override
-      public DocContainerBuilder externalId(String externalId) {
+      public CreateDoc externalId(String externalId) {
         return this;
       }
       @Override
-      public DocContainerBuilder draftedBy(String draftedBy) {
+      public CreateDoc draftedBy(String draftedBy) {
         return this;
       }
       @Override
-      public DocContainerBuilder decidedBy(String decidedBy) {
+      public CreateDoc decidedBy(String decidedBy) {
         return this;
       }
       @Override
-      public DocContainerBuilder createdBy(String createdBy) {
+      public CreateDoc createdBy(String createdBy) {
         return this;
       }
       @Override
-      public DocContainerBuilder props(Map<String, String> props) {
+      public CreateDoc props(Map<String, String> props) {
         return this;
       }
       @Override
@@ -66,11 +73,105 @@ public class DocContainerClientDummy implements DocContainerClient {
             .build());
       }
       @Override
-      public DocContainerBuilder addDocument(Doc newDocument) {
+      public CreateDoc addDocument(Doc newDocument) {
         docsToMerge.add(newDocument);
+        return this;
+      }
+      @Override
+      public CreateDoc task(Task task) {
+        this.task = task;
         return this;
       }
 
     };
   }
+
+  @Override
+  public DocContainerTasUpdater updateTask() {
+    return new DocContainerTasUpdater() {
+      private MergeMission merge;
+      private DocContainer doc;
+      private String userId;
+      
+      @Override
+      public DocContainerTasUpdater task(MergeMission task) {
+        this.merge = task;
+        return this;
+      }
+      
+      @Override
+      public DocContainerTasUpdater doc(DocContainer doc) {
+        this.doc = doc;
+        return this;
+      }
+
+      @Override
+      public DocContainerTasUpdater userId(String userId) {
+        this.userId = userId;
+        return this;
+      }
+      
+      @Override
+      public void build() {
+
+        final var docContainerId = doc.getId();
+        final var props = doc.getProps();
+        final var files = doc.getDocs().values();
+        
+        final var linkBody = JsonObject.mapFrom(props);
+        linkBody.put("files", files.stream().map(e -> e.getName()).toList());
+        
+        
+        final var previousTransfer = merge.getCurrentState().getLinks().values().stream()
+          .filter(e -> TaskMapper.LINK_TYPE_TRANSFERRED_ID.equals(e.getLinkType()))
+          .findFirst();
+        
+        if(previousTransfer.isEmpty()) {
+          merge.addLink(newLink -> newLink
+            .linkType(TaskMapper.LINK_TYPE_TRANSFERRED_ID)
+            .linkValue(docContainerId)
+            .linkBody(linkBody)
+            .build());
+        } else {
+          merge.modifyLink(previousTransfer.get().getId(), modLink -> {
+            modLink
+              .linkType(TaskMapper.LINK_TYPE_TRANSFERRED_ID)
+              .linkValue(docContainerId)
+              .linkBody(linkBody)
+              .build();
+          });
+        }
+        
+        merge.status(TaskStatus.TRANSFERRED.name())
+        // change is viewed by worker who created it
+        .addViewer(viewer -> viewer.userId(userId).usedFor(TaskMapper.VIEWER_WORKER).currentTxCommit().build())
+        .build();
+        
+        
+        if(previousTransfer.isEmpty()) {
+          merge.addLink(newLink -> newLink
+            .linkType(TaskMapper.LINK_TYPE_TRANSFERRED_ID)
+            .linkValue(docContainerId)
+            .linkBody(linkBody)
+            .build());
+        } else {
+          merge.modifyLink(previousTransfer.get().getId(), modLink -> {
+            modLink
+              .linkType(TaskMapper.LINK_TYPE_TRANSFERRED_ID)
+              .linkValue(docContainerId)
+              .linkBody(linkBody)
+              .build();
+          });
+        }
+        
+        merge.status(TaskStatus.TRANSFERRED.name())
+        // change is viewed by worker who created it
+        .addViewer(viewer -> viewer.userId(userId).usedFor(TaskMapper.VIEWER_WORKER).currentTxCommit().build())
+        .build();
+        
+      }
+
+    };
+  }
+
 }
