de5a464ff8473dbd452cb5ad1e56b7dfc5a465de
jocelyn.mutso <jocelyn.mutso@resys.io>
2026-05-05 10:53:58 +0300

    #672 clean up

--- ts_setup/modules/fs-composer/fs-dirent-select-single/FsDirentSelectSingle.tsx
+++ ts_setup/modules/fs-composer/fs-dirent-select-single/FsDirentSelectSingle.tsx
@@ -8,13 +8,17 @@ export const FsDirentSelectSingle: React.FC<FsDirentSelectSingleProps> = (props)
   const ownerState = useOwnerState(props);
   const classes = useUtilityClasses();
 
+  const value = props.options.some(o => o.value === props.value) ? props.value : '';
+
   return (
     <FsDirentSelectSingleRoot className={classes.root} ownerState={ownerState} fullWidth size='small'>
       <Select className={classes.select}
-        value={props.value}
+        value={value}
+        displayEmpty
         onChange={(e) => props.onChange(e.target.value as string)}
         input={<OutlinedInput />}
       >
+        <MenuItem value='' />
         {props.options.map((option) => (
           <MenuItem key={option.value} value={option.value}>
             {option.label}
