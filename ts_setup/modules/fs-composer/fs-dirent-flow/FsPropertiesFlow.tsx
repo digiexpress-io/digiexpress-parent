47b1cc32451da452ecd2e498addeb278d2de4181
jocelyn.mutso <jocelyn.mutso@resys.io>
2026-06-25 13:04:31 +0300

    #763 fixed duplicate rendering of flow properties

--- ts_setup/modules/fs-composer/fs-dirent-flow/FsPropertiesFlow.tsx
+++ ts_setup/modules/fs-composer/fs-dirent-flow/FsPropertiesFlow.tsx
@@ -1,44 +1,17 @@
 import React from 'react';
-import { Typography } from '@mui/material';
-import { useIntl } from 'react-intl';
 import { useFsDirent } from '@dxs-ts/fs-api';
-import { usePanelProperties } from '../fs-panel-properties';
 
 export interface FsPropertiesFlowProps {
   direntId: string;
 }
 
 export const FsPropertiesFlow: React.FC<FsPropertiesFlowProps> = ({ direntId }) => {
-  const intl = useIntl();
-  const classes = usePanelProperties();
   const { getDirent } = useFsDirent();
-
   const dirent = getDirent(direntId);
 
   if (!dirent || dirent.type !== 'FLOW') {
     return undefined;
   }
 
-  const description = dirent.props?.assetDescription;
-  const labels = dirent.props?.labels ?? [];
-
-  return (
-    <>
-      <div className={classes.propertyRow}>
-        <Typography className={classes.propertyLabel}>{intl.formatMessage({ id: 'fs.properties.propertyLabel.description' })}</Typography>
-        <Typography className={classes.propertyValue}>{description}</Typography>
-      </div>
-
-      <div className={classes.propertyRow}>
-        <Typography className={classes.propertyLabel}>{intl.formatMessage({ id: 'fs.properties.propertyLabel.labels' })}</Typography>
-        <div className={classes.propertyList}>
-          {labels.map((label, index) => (
-            <div key={index} className={classes.label}>
-              <Typography component="span">{label.key}</Typography>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 };
