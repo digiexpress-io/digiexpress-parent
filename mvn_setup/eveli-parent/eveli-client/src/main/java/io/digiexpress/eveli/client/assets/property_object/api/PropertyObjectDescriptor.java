6c245d04804db94e5f9c293129a5ebb304acdc98
Vahur Krouverk <vahur.krouverk@resys.io>
2026-08-06 13:44:26 +0300

    issue #766 generalized property object descriptor

--- mvn_setup/eveli-parent/eveli-client/src/main/java/io/digiexpress/eveli/client/assets/property_object/api/PropertyObjectDescriptor.java
+++ mvn_setup/eveli-parent/eveli-client/src/main/java/io/digiexpress/eveli/client/assets/property_object/api/PropertyObjectDescriptor.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.module.jsonSchema.jakarta.JsonSchema;
 import io.resys.limaone.model.Model;
 import io.resys.limaone.model.PropertyObject;
 
-public interface PropertyObjectDescriptor <T extends BasePropertyObject> {
+public interface PropertyObjectDescriptor <T> {
   T convertToObject(Model<PropertyObject> m);
   String getPropertyObjectType();
   JsonSchema getSchema(); // if property object is based on JSON then it will provide schema, otherwise null
