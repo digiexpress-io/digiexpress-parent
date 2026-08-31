25d7e9efea524da55db3ab0004373700af5923b7
Olev Mutso <olev.mutso@resys.io>
2026-08-31 12:53:16 +0300

    merged

--- ts_setup/modules/lib-gamut/version.ts
+++ ts_setup/modules/lib-gamut/version.ts
@@ -1,17 +1,9 @@
 import { VersionInfoApi } from "@dxs-ts/envir-util";
-<<<<<<< HEAD
-export const version = '2.0.77';export const build_time = '08/28/2026, 08:09:58';
-export const renderVersion = () => VersionInfoApi.builder()
-          .setLogo('logo_1_great_ones_wisdom')
-          .setTheme('red')
-          .setProjectInfo('@dxs-ts/gamut', '2.0.77', '08/28/2026, 08:09:58')
-=======
 export const version = '2.0.76';export const build_time = '08/31/2026, 12:06:05';
 export const renderVersion = () => VersionInfoApi.builder()
           .setLogo('logo_1_great_ones_wisdom')
           .setTheme('red')
           .setProjectInfo('@dxs-ts/gamut', '2.0.76', '08/31/2026, 12:06:05')
->>>>>>> 1cc6101b8 (deleted old deps)
           .addInternalComponents(['@dxs-ts/envir-util','@dxs-ts/gamut-cockpit-store','@dxs-ts/gamut-api','@dxs-ts/gamut-md','@dxs-ts/gamut-primitives','@dxs-ts/gamut-form-review','@dxs-ts/gamut-form','@dxs-ts/gamut-shell','@dxs-ts/gamut-intl','@dxs-ts/gamut-routes','@dxs-ts/gamut-theme','@dxs-ts/xui-datetime'])
           .addExternalComponents(['@mui/icons-material@6.5.0','@mui/material@6.5.0','@mui/system@6.5.0','@mui/utils@6.4.9','@tanstack/react-query@5.83.0','@tanstack/react-router@1.130.2','@tanstack/react-store@0.11.0','leaflet-geosearch@4.2.0','luxon@3.7.1','numbro@2.5.0','react@18.3.1','react-intl@6.8.9','react-leaflet@4.2.1','react-markdown@10.1.0','ts-md5@2.0.1'])
           .render();
